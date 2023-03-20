30830ddd173c9a669e26012e861a365ad2d39fd2
ispong <ispong@outlook.com>
2023-03-20 15:11:10 +0800

    :sparkles: 初始化gradle项目

---
+++ src/test/java/com/isxcode/star/sparkyunbackend/SparkYunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isxcode.star.sparkyunbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkYunBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
