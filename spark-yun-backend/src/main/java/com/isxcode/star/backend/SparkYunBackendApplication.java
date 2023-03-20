78848c47d144bf366ac9072327d6535ce18032b3
ispong <ispong@outlook.com>
2023-03-20 19:27:30 +0800

    :sparkles: gradle自定义任务可以执行后台和代理运行测试

---
+++ spark-yun-backend/src/main/java/com/isxcode/star/backend/SparkYunBackendApplication.java
@@ -0,0 +1,12 @@
+package com.isxcode.star.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkYunBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkYunBackendApplication.class, args);
+    }
+}
