d6c6cc6d16ea73195f71987fa414419e66a65260
ispong <ispong@outlook.com>
2023-03-23 23:12:56 +0800

    :sparkles: 前端项目添加ant-design和webpack

---
+++ spark-yun-website/src/layouts/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button} from "antd";
+
+function App() {
+    return (
+        <div className="App">
+            <Button>hello</Button>
+        </div>
+    );
+}
+
+export default App;
