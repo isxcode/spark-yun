1baf423adc0d7eea400e4b790e6ce17c28945ff6
ispong <ispong@outlook.com>
2023-03-23 11:14:06 +0800

    :sparkles: 使用GenericGenerator初始化id

--- spark-yun-backend/src/main/java/com/isxcode/star/backend/module/user/entity/UserEntity.java
+++ spark-yun-backend/src/main/java/com/isxcode/star/backend/module/user/entity/UserEntity.java
@@ -2,9 +2,11 @@ package com.isxcode.star.backend.module.user.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 /** 只负责数据库对象映射. */
 @Data
@@ -13,7 +15,12 @@ import lombok.Data;
 @JsonIgnoreProperties({"hibernateLazyInitializer"})
 public class UserEntity {
 
-  @Id private String id;
+  @Id
+  @GeneratedValue(generator = "sy-id-generator")
+  @GenericGenerator(
+      name = "sy-id-generator",
+      strategy = "com.isxcode.star.backend.config.GeneratedValueConfig")
+  private String id;
 
   private String account;
 
