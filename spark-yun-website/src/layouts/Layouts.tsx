52d30bf039533fc20d8006c7ee64c65a1be8ffd3
ispong <ispong@outlook.com>
2023-03-29 15:33:42 +0800

    ✨ 前端模块化 #19
    * :sparkles: 前端项目添加ant-design和webpack
    
    * :bug: 修复前端打包无法让后端运行的问题
    
    * :sparkles: 前端添加eslint和prettier配置
    
    * :sparkles: 集成gradle打包生效
    
    * :sparkles: 前端代码格式和代码质量检测完成
    
    * :sparkles: 添加scss和ant-design和router
    
    * :sparkles: 修复前端代码样式

---
+++ spark-yun-website/src/layouts/Layouts.tsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import { Layout, Menu, type MenuProps, message, theme } from 'antd'
+import { Outlet, useNavigate } from 'react-router-dom'
+
+function Layouts () {
+  const navigate = useNavigate()
+
+  const { Header, Content, Sider } = Layout
+
+  const menus: MenuProps['items'] = [
+    {
+      key: 1,
+      label: '首页',
+      onClick: () => {
+        navigate('/monitor')
+      }
+    },
+    {
+      key: 2,
+      label: '项目管理',
+      onClick: () => {
+        navigate('/project')
+      }
+    },
+    {
+      key: 3,
+      label: '计算集群',
+      onClick: message.warning('请上传企业许可证！')
+    },
+    {
+      key: 4,
+      label: '数据源',
+      onClick: () => {
+        navigate('/datasource')
+      }
+    },
+    {
+      key: 5,
+      label: '作业流',
+      onClick: () => {
+        navigate('/work')
+      }
+    },
+    {
+      key: 6,
+      label: '数据建模',
+      onClick: message.warning('请上传企业许可证！')
+    },
+    {
+      key: 7,
+      label: '调度历史',
+      onClick: message.warning('请上传企业许可证！')
+    },
+    {
+      key: 8,
+      label: 'BI酷屏',
+      onClick: message.warning('请上传企业许可证！')
+    },
+    {
+      key: 9,
+      label: '数据资产',
+      onClick: message.warning('请上传企业许可证！')
+    },
+    {
+      key: 10,
+      label: '自定义Api',
+      onClick: message.warning('请上传企业许可证！')
+    },
+    {
+      key: 11,
+      label: 'AI计算',
+      onClick: message.warning('请上传企业许可证！')
+    },
+    {
+      key: 12,
+      label: '后台管理',
+      onClick: message.warning('请上传企业许可证！')
+    },
+    {
+      key: 13,
+      label: '用户中心',
+      onClick: message.warning('请上传企业许可证！')
+    },
+    {
+      key: 14,
+      label: '系统配置',
+      onClick: message.warning('请上传企业许可证！')
+    }
+  ]
+
+  const {
+    token: { colorBgContainer, colorPrimary }
+  } = theme.useToken()
+
+  return (
+    <>
+      <Layout>
+        <Header style={{ background: colorBgContainer, borderBottom: colorPrimary + ' solid 3px' }}>
+          <div
+            style={{
+              background: colorPrimary,
+              color: 'white',
+              width: '100px',
+              textAlign: 'center',
+              fontSize: '18px'
+            }}>
+            至轻云
+          </div>
+        </Header>
+        <Layout>
+          <Sider width={200} style={{ background: colorBgContainer }}>
+            <Menu defaultSelectedKeys={['1']} items={menus} />
+          </Sider>
+          <Layout style={{ padding: '16px 24px' }}>
+            <Content
+              style={{
+                padding: 24,
+                margin: 0,
+                background: colorBgContainer
+              }}>
+              <Outlet />
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    </>
+  )
+}
+
+export default Layouts
