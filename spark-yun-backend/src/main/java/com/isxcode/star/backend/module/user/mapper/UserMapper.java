90556e4183daa0cd1e414ff65cff42fa1291f4b8
ispong <ispong@outlook.com>
2023-03-29 15:03:52 +0800

    🔖  发版0.0.3 (#19)
    * :fire: 清空前端gradle的内容
    
    * :sparkles: 规范后端模块开发
    
    * :sparkles: 规范后端模块开发-添加mapstruct
    
    * :sparkles: 规范后端模块开发-添加spotless代码格式检查
    
    * :sparkles: 规范后端模块开发-添加checkstyle检查
    
    * :sparkles: 规范后端模块开发-添加checkstyle检查
    
    * :sparkles: 规范后端模块开发-添加checkstyle检查
    
    * :sparkles: 添加数据源连接池配置
    
    * :sparkles: 强制开启checkstyle检查
    
    * :sparkles: 添加javadocs
    
    * :sparkles: 使用GenericGenerator初始化id
    
    * :sparkles: 使用druid管理数据源连接池
    
    * :fire: 删除checkstyle无用配置文件

---
+++ spark-yun-backend/src/main/java/com/isxcode/star/backend/module/user/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.isxcode.star.backend.module.user.mapper;
+
+import com.isxcode.star.api.pojos.user.req.AddUserReq;
+import com.isxcode.star.backend.module.user.entity.UserEntity;
+import org.mapstruct.Mapper;
+
+/** mapstruct映射. */
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+  UserEntity addUserReqToUserEntity(AddUserReq addUserReq);
+}
---
+++ spark-yun-backend/src/main/java/com/isxcode/star/backend/module/user/service/UserService.java
@@ -0,0 +1,36 @@
+package com.isxcode.star.backend.module.user.service;
+
+import com.isxcode.star.backend.module.user.entity.UserEntity;
+import com.isxcode.star.backend.module.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/** 解藕出来的基础逻辑. */
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+  private final UserRepository userRepository;
+
+  /*
+   * 判断用户是否存在
+   *
+   * @param account 用户账号
+   * @return true为存在 false为不存在
+   * @ispong
+   */
+  public Boolean userExist(String account) {
+
+    return userRepository.findFirstByAccount(account) != null;
+  }
+
+  /*
+   * 添加用户
+   *
+   * @ispong
+   */
+  public void addUser(UserEntity user) {
+
+    userRepository.save(user);
+  }
+}
