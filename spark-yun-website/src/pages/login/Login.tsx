40697c36dc2dbf93927ee7f326d3818ef54bcb5c
ispong <ispong@outlook.com>
2023-03-29 14:19:03 +0800

    :sparkles: 修复前端代码样式

--- spark-yun-website/src/pages/login/Login.tsx
+++ spark-yun-website/src/pages/login/Login.tsx
@@ -1,62 +1,52 @@
-import React from 'react';
-import './Login.scss';
-import {Button, Checkbox, Form, Input, theme} from "antd";
-function Login() {
-
+import React from 'react'
+import './Login.scss'
+import { Button, Form, Input, theme } from 'antd'
+function Login () {
   const onFinish = (values: any) => {
-    console.log('Success:', values);
-  };
+    console.log('Success:', values)
+  }
 
   const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-  };
+    console.log('Failed:', errorInfo)
+  }
 
   const {
-    token: { colorPrimary},
-  } = theme.useToken();
+    token: { colorPrimary }
+  } = theme.useToken()
 
-  return <div className={"sy-login-page"}>
+  return (
+    <div className={'sy-login-page'}>
+      <div className={'sy-login-div'}>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off">
+          <div className={'sy-login-title'} style={{ background: colorPrimary }}>
+            至轻云
+          </div>
 
-    <div className={'sy-login-div'}>
-      <Form
-        name="basic"
-        labelCol={{span: 8}}
-        wrapperCol={{span: 16}}
-        style={{maxWidth: 600}}
-        initialValues={{remember: true}}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
+          <Form.Item name="account" rules={[{ required: true, message: '账号不能为空' }]}>
+            <Input className={'sy-login-account-input'} placeholder="账号/邮箱/手机号" />
+          </Form.Item>
 
-        <div className={'sy-login-title'} style={{background: colorPrimary}}>
-          至轻云
-        </div>
+          <Form.Item name="password" rules={[{ required: true, message: '密码不能为空' }]}>
+            <Input.Password className={'sy-login-password-input'} placeholder="密码" />
+          </Form.Item>
 
-        <Form.Item
-          name="account"
-          rules={[{required: true, message: '账号不能为空'}]}
-        >
-          <Input className={'sy-login-account-input'} placeholder="账号/邮箱/手机号"/>
-        </Form.Item>
-
-        <Form.Item
-
-          name="password"
-          rules={[{required: true, message: '密码不能为空'}]}
-        >
-          <Input.Password className={'sy-login-password-input'} placeholder="密码" />
-        </Form.Item>
-
-        <Form.Item wrapperCol={{offset: 8, span: 16}}>
-          <Button className={'sy-login-btn'} type="primary" htmlType="submit">
-            登录
-          </Button>
-        </Form.Item>
-      </Form>
-
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button className={'sy-login-btn'} type="primary" htmlType="submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
-  </div>;
+  )
 }
 
-export default Login;
+export default Login
--- spark-yun-website/src/pages/monitor/Monitor.tsx
+++ spark-yun-website/src/pages/monitor/Monitor.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-function Monitor() {
-
-  return <>Monitor</>;
+function Monitor () {
+  return <>Monitor</>
 }
 
-export default Monitor;
+export default Monitor
