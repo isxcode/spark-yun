4c96d17e60a6c093fa8869bb83ac1a03ed03038c
ispong <ispong@outlook.com>
2023-04-27 15:22:36 +0800

    :sparkles: 添加许可证模块

---
+++ spark-yun-api/src/main/java/com/isxcode/star/api/pojos/license/req/LicQueryLicenseReq.java
@@ -0,0 +1,8 @@
+package com.isxcode.star.api.pojos.license.req;
+
+import com.isxcode.star.api.pojos.base.BasePageRequest;
+import lombok.Data;
+
+@Data
+public class LicQueryLicenseReq extends BasePageRequest {
+}
