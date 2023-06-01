5c64b274a2a46a5867ede306fb731f656afe7918
ispong <ispong@outlook.com>
2023-06-01 15:48:06 +0800

    :boom: 集成灿的vue3项目

--- spark-yun-website/src/router/index.ts
+++ spark-yun-website/src/router/index.ts
@@ -1,23 +1,48 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+/*
+ * @Author: fanciNate
+ * @Date: 2023-04-17 09:43:00
+ * @LastEditTime: 2023-04-27 17:12:59
+ * @LastEditors: fanciNate
+ * @Description: In User Settings Edit
+ * @FilePath: /zqy-web/src/router/index.ts
+ */
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/home/index.vue";
+import Login from "@/views/login/login.vue";
+
+import HomeChildren from "./home-children";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    redirect: {
+      name: "home",
+    },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: Home,
+    children: HomeChildren,
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-  ],
+  routes,
 });
 
 export default router;
