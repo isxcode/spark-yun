6e22f8101e4ee97c283d1586edd942eb6bab94ec
ispong <ispong@outlook.com>
2023-06-09 19:07:08 +0800

    :sparkles: 测试k8s集成

--- spark-yun-agent/src/main/java/com/isxcode/star/yun/agent/run/YunAgentBizService.java
+++ spark-yun-agent/src/main/java/com/isxcode/star/yun/agent/run/YunAgentBizService.java
@@ -2,7 +2,6 @@ package com.isxcode.star.yun.agent.run;
 
 import com.alibaba.fastjson.JSON;
 import com.isxcode.star.api.exceptions.SparkYunException;
-//import com.isxcode.star.api.pojos.yun.agent.req.SparkLauncher;
 import com.isxcode.star.api.pojos.yun.agent.req.YagExecuteWorkReq;
 import com.isxcode.star.api.pojos.yun.agent.res.YagExecuteWorkRes;
 import com.isxcode.star.api.pojos.yun.agent.res.YagGetDataRes;
@@ -14,17 +13,34 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.yarn.api.records.ApplicationReport;
 import org.apache.hadoop.yarn.client.api.YarnClient;
 import org.apache.hadoop.yarn.exceptions.YarnException;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.apache.logging.log4j.util.Strings;
 import org.apache.spark.launcher.SparkLauncher;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
-import java.util.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static com.isxcode.star.yarn.utils.YarnUtils.formatApplicationId;
 import static com.isxcode.star.yarn.utils.YarnUtils.initYarnClient;
+import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
 
 /**
  * 代理服务层.
@@ -42,6 +58,7 @@ public class YunAgentBizService {
     SparkLauncher sparkLauncher = new SparkLauncher()
       .setVerbose(yagExecuteWorkReq.getSparkSubmit().isVerbose())
       .setMainClass(yagExecuteWorkReq.getSparkSubmit().getMainClass())
+      .setDeployMode(yagExecuteWorkReq.getSparkSubmit().getDeployMode())
       .setMaster(yagExecuteWorkReq.getSparkSubmit().getMaster())
       .setAppName(yagExecuteWorkReq.getSparkSubmit().getAppName())
       .setAppResource(yagExecuteWorkReq.getSparkSubmit().getAppResource())
@@ -69,7 +86,42 @@ public class YunAgentBizService {
 
     yagExecuteWorkReq.getSparkSubmit().getConf().forEach(sparkLauncher::setConf);
 
-    sparkLauncher.launch("123");
+    Process launch = sparkLauncher.launch();
+    InputStream inputStream = launch.getErrorStream();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
+    // 等待作业响应
+    String applicationId;
+
+    StringBuilder errLog = new StringBuilder();
+    int errIndex = 0;
+    boolean isError = false;
+
+    // 如果发现错误日志，返回100行日志
+    String line;
+    while ((line = reader.readLine()) != null) {
+
+      if (errIndex > 100) {
+        throw new SparkYunException(errLog.toString());
+      }
+
+      if (!isError) {
+        if ((containsIgnoreCase(line, "Error") || containsIgnoreCase(line, "Exception")) && !line.contains("at ")) {
+          isError = true;
+        }
+      }
+
+      if (isError) {
+        errLog.append(line).append("\n");
+        errIndex++;
+      }
+
+      System.out.println(line);
+    }
+
+    if (launch.exitValue() == 1) {
+      throw new SparkYunException(errLog.toString());
+    }
 
     return null;
   }
@@ -135,5 +187,123 @@ public class YunAgentBizService {
     }
   }
 
+  public String getStatus1(String jobName) throws IOException {
+
+    // 爬虫获取状态
+    Document doc = Jsoup.connect("http://39.100.67.15:8081/").get();
+
+    Element completedAppsTable = doc.selectFirst("div.aggregated-completedApps table");
+    Elements completedRows = completedAppsTable.select("tbody tr");
+
+    Element activeAppsTable = doc.selectFirst("div.aggregated-activeApps table");
+    Elements activeRows = activeAppsTable.select("tbody tr");
+
+    Map<String, String> apps = new HashMap<>();
+
+    for (Element row : completedRows) {
+      apps.put(row.select("td").get(1).text(), row.select("td").get(7).text());
+    }
+
+    for (Element row : activeRows) {
+      apps.put(row.select("td").get(1).text(), row.select("td").get(7).text());
+    }
+
+    return apps.get(jobName);
+  }
+
+  public String getAppId(String jobName) throws IOException {
+
+    // 爬虫获取状态
+    Document doc = Jsoup.connect("http://39.100.67.15:8081/").get();
+
+    Element completedAppsTable = doc.selectFirst("div.aggregated-completedApps table");
+    Elements completedRows = completedAppsTable.select("tbody tr");
+
+    Element activeAppsTable = doc.selectFirst("div.aggregated-activeApps table");
+    Elements activeRows = activeAppsTable.select("tbody tr");
+
+    Map<String, String> apps = new HashMap<>();
+
+    for (Element row : completedRows) {
+      apps.put(row.select("td").get(1).text(), row.select("td").get(0).text());
+    }
+
+    for (Element row : activeRows) {
+      apps.put(row.select("td").get(1).text(), row.select("td").get(0).text().replace("(kill)", ""));
+    }
+
+    return apps.get(jobName);
+  }
+
+  public String getOutUrl() throws IOException {
+
+    // 爬虫获取状态
+    Document doc = Jsoup.connect("http://39.100.67.15:8081/app/?appId=app-20230608171856-0001").get();
+
+    // 提取stdout链接
+    Elements stdoutLinks = doc.select("div.aggregated-removedExecutors tbody tr td:nth-child(8) a[href]:eq(0)");
+    for (Element link : stdoutLinks) {
+     return  link.attr("href");
+    }
+    return null;
+  }
+
+  public String getErrUrl() throws IOException {
+
+    // 爬虫获取状态
+    Document doc = Jsoup.connect("http://39.100.67.15:8081/app/?appId=app-20230608171856-0001").get();
+
+    // 提取stderr链接
+    Elements stderrLinks = doc.select("div.aggregated-removedExecutors tbody tr td:nth-child(8) a[href]:eq(1)");
+    for (Element link : stderrLinks) {
+      return link.attr("href");
+    }
+    return null;
+  }
+
+  public String getErrLog() throws IOException {
+
+    Document doc = Jsoup.connect("http://39.100.67.15:8082/logPage/?appId=app-20230608171856-0001&executorId=0&logType=stderr").get();
+
+    Element preElement = doc.selectFirst("pre");
+
+    return preElement.text();
+  }
+
+  public String getOutLog() throws IOException {
+
+    Document doc = Jsoup.connect("http://39.100.67.15:8082/logPage/?appId=app-20230608171856-0001&executorId=0&logType=stdout").get();
+
+    Element preElement = doc.selectFirst("pre");
+
+    return preElement.text();
+  }
+
+  public void killWork() throws IOException, ParseException {
+
+    String url = "http://39.100.67.15:8081/app/kill/";
+
+    CloseableHttpClient httpClient = HttpClients.createDefault();
+    HttpPost httpPost = new HttpPost(url);
+
+    String payload = "id=app-20230608190523-0016&terminate=true";
+    StringEntity stringEntity = new StringEntity(payload, ContentType.APPLICATION_FORM_URLENCODED);
+
+    httpPost.setEntity(stringEntity);
+
+    try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+      // 获取响应实体
+      HttpEntity responseEntity = response.getEntity();
+      if (responseEntity != null) {
+        String responseBody = EntityUtils.toString(responseEntity);
+        System.out.println(responseBody);
+      }
+    }
+  }
+
+  public static void main(String[] args) throws IOException, ParseException {
+
+
+  }
 
 }
--- spark-yun-plugins/spark-query-sql-plugin/src/main/java/com/isxcode/star/plugin/query/sql/Execute.java
+++ spark-yun-plugins/spark-query-sql-plugin/src/main/java/com/isxcode/star/plugin/query/sql/Execute.java
@@ -1,7 +1,7 @@
 package com.isxcode.star.plugin.query.sql;
 
-import com.alibaba.fastjson.JSON;
-import com.isxcode.star.api.pojos.plugin.req.PluginReq;
+//import com.alibaba.fastjson.JSON;
+//import com.isxcode.star.api.pojos.plugin.req.PluginReq;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
@@ -18,42 +18,30 @@ public class Execute {
   /** 最后一句sql查询. */
   public static void main(String[] args) {
 
-    PluginReq pluginReq = parse(args);
+    try (SparkSession sparkSession = initSparkSession()) {
 
-    String regex = "/\\*(?:.|[\\n\\r])*?\\*/|--.*";
-    String noCommentSql = pluginReq.getSql().replaceAll(regex, "");
-    String realSql = noCommentSql.replace("\n", " ");
-    String[] sqls = realSql.split(";");
-
-    try (SparkSession sparkSession = initSparkSession(pluginReq)) {
-
-      for (int i = 0; i < sqls.length - 1; i++) {
-        if (!Strings.isEmpty(sqls[i])) {
-          sparkSession.sql(sqls[i]);
-        }
-      }
-      Dataset<Row> rowDataset = sparkSession.sql(sqls[sqls.length - 1]).limit(pluginReq.getLimit());
+      Dataset<Row> rowDataset = sparkSession.sql("show databases").limit(100);
       exportResult(rowDataset);
     }
   }
 
-  public static PluginReq parse(String[] args) {
-    if (args.length == 0) {
-      throw new RuntimeException("args is empty");
-    }
-    return JSON.parseObject(Base64.getDecoder().decode(args[0]), PluginReq.class);
-  }
+//  public static PluginReq parse(String[] args) {
+//    if (args.length == 0) {
+//      throw new RuntimeException("args is empty");
+//    }
+//    return JSON.parseObject(Base64.getDecoder().decode(args[0]), PluginReq.class);
+//  }
 
-  public static SparkSession initSparkSession(PluginReq pluginReq) {
+  public static SparkSession initSparkSession() {
 
     SparkSession.Builder sparkSessionBuilder = SparkSession.builder();
 
     SparkConf conf = new SparkConf();
-    if (pluginReq.getSparkConfig() != null) {
-      for (Map.Entry<String, String> entry : pluginReq.getSparkConfig().entrySet()) {
-        conf.set(entry.getKey(), entry.getValue());
-      }
-    }
+//    if (pluginReq.getSparkConfig() != null) {
+//      for (Map.Entry<String, String> entry : pluginReq.getSparkConfig().entrySet()) {
+//        conf.set(entry.getKey(), entry.getValue());
+//      }
+//    }
 
     return sparkSessionBuilder.config(conf).enableHiveSupport().getOrCreate();
   }
@@ -64,6 +52,7 @@ public class Execute {
 
     // 表头
     result.add(Arrays.asList(rowDataset.columns()));
+
 
     // 数据
     rowDataset
@@ -77,6 +66,8 @@ public class Execute {
               result.add(metaData);
             });
 
-    System.out.println(JSON.toJSONString(result));
+    System.out.println("==============");
+    System.out.println(result);
+    System.out.println("==============");
   }
 }
