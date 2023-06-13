78f80ffc7cc456ff0824b1b18b83cef70a5ad29d
ispong <ispong@outlook.com>
2023-06-13 14:44:16 +0800

    :sparkles: 测试k8s作业提交

--- spark-yun-agent/src/main/java/com/isxcode/star/yun/agent/service/AgentService.java
+++ spark-yun-agent/src/main/java/com/isxcode/star/yun/agent/service/AgentService.java
@@ -7,14 +7,14 @@ import org.apache.spark.launcher.SparkLauncher;
 
 public interface AgentService {
 
-  String getMaster();
+  String getMaster() throws IOException;
 
   /**
    * @param pluginReq 插件请求体
    * @param sparkSubmit spark作业提交配置
    */
   SparkLauncher genSparkLauncher(
-      PluginReq pluginReq, SparkSubmit sparkSubmit, String agentHomePath);
+      PluginReq pluginReq, SparkSubmit sparkSubmit, String agentHomePath) throws IOException;
 
   /** @param sparkLauncher spark启动器 */
   String executeWork(SparkLauncher sparkLauncher) throws IOException;
