5b489fabefc6e79f6d27a33192f7a718adb8abd6
ispong <ispong@outlook.com>
2023-06-24 11:21:38 +0800

    :bug: 修复没有权限访问接口，返回200改为返回401

--- spark-yun-common/src/main/java/com/isxcode/star/common/advice/GlobalExceptionAdvice.java
+++ spark-yun-common/src/main/java/com/isxcode/star/common/advice/GlobalExceptionAdvice.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -55,6 +56,18 @@ public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
       SuccessResponseException successException) {
 
     return new ResponseEntity<>(successException.getBaseResponse(), HttpStatus.OK);
+  }
+
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<BaseResponse<Object>> accessDeniedException(
+      AccessDeniedException accessDeniedException) {
+
+    BaseResponse baseResponse = new BaseResponse();
+    baseResponse.setCode("401");
+    baseResponse.setMsg("当前用户没有权限");
+    baseResponse.setErr(accessDeniedException.getMessage());
+
+    return new ResponseEntity<>(baseResponse, HttpStatus.OK);
   }
 
   @ExceptionHandler(EmptyResultDataAccessException.class)
