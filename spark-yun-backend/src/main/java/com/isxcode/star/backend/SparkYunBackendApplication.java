0fd840bbf06f2dd48da5ed55ba0c227e83d87ed9
ispong <ispong@outlook.com>
2023-06-24 11:21:38 +0800

    :bug: 修复没有权限访问接口，返回200改为返回401

--- spark-yun-backend/src/main/java/com/isxcode/star/backend/SparkYunBackendApplication.java
+++ spark-yun-backend/src/main/java/com/isxcode/star/backend/SparkYunBackendApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/")
