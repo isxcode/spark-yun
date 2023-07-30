8b8fc18406c18eaceec84507905b61c9d31b9463
fancinate <1585546519@qq.com>
2023-07-30 16:06:26 +0800

    :zap: 添加配置抽屉

---
+++ spark-yun-website/src/components/zqy-log/index.vue
@@ -0,0 +1,148 @@
+<template>
+    <BlockModal :model-config="modelConfig" @close="closeEvent">
+        <div id="content" class="content-box">
+            <!-- 日志展示 -->
+            <pre v-if="logMsg" ref="preContentRef" @mousewheel="mousewheelEvent">{{ logMsg }}</pre>
+        </div>
+    </BlockModal>
+</template>
+  
+<script lang="ts" setup>
+import { reactive, ref, nextTick, onUnmounted } from 'vue'
+import BlockModal from '@/components/block-modal/index.vue'
+
+import { GetLogData, GetYarnLogData } from '@/services/schedule.service'
+
+const callback = ref<any>()
+const logMsg = ref('')
+const info = ref('')
+const modalType = ref('')
+const position = ref(false)
+const timer = ref(null)
+const preContentRef = ref(null)
+
+const modelConfig = reactive({
+    title: '日志',
+    visible: false,
+    width: '820px',
+    cancelConfig: {
+        title: '关闭',
+        cancel: closeEvent,
+        disabled: false
+    },
+    needScale: false,
+    zIndex: 1100,
+    customClass: 'zqy-log-modal',
+    closeOnClickModal: false
+})
+
+function showModal(cb: () => void, data: any, type: string): void {
+    callback.value = cb
+    info.value = data.id
+    modalType.value = type
+
+    if (modalType.value === 'log') {
+        position.value = true
+        getLogData()
+        if (!timer.value) {
+            timer.value = setInterval(() => {
+                getLogData()
+            }, 3000)
+        }
+        modelConfig.title = '日志'
+    } else if (modalType.value === 'result') {
+        getResultDatalist()
+        modelConfig.width = '64%'
+        modelConfig.title = '结果'
+    } else if (modalType.value === 'yarnLog') {
+        position.value = true
+        modelConfig.title = '运行日志'
+        getYarnLogData()
+    }
+    modelConfig.visible = true
+}
+
+// 获取日志
+function getLogData() {
+    GetLogData({
+        instanceId: info.value
+    })
+        .then((res: any) => {
+            logMsg.value = res.data.log
+            if (position.value) {
+                nextTick(() => {
+                    scrollToButtom()
+                })
+            }
+        })
+        .catch(() => {
+            logMsg.value = ''
+        })
+}
+
+// 获取yarn日志
+function getYarnLogData() {
+    GetYarnLogData({
+        instanceId: info.value
+    })
+        .then((res: any) => {
+            logMsg.value = res.data.yarnLog
+        })
+        .catch(() => {
+            logMsg.value = ''
+        })
+}
+
+function scrollToButtom() {
+    if (preContentRef.value) {
+        document.getElementById('content').scrollTop = preContentRef.value.scrollHeight // 滚动高度
+    }
+}
+
+function mousewheelEvent(e: any) {
+    if (!(e.deltaY > 0)) {
+        position.value = false
+    }
+}
+
+function closeEvent() {
+    if (timer.value) {
+        clearInterval(timer.value)
+    }
+    timer.value = null
+    modelConfig.visible = false
+}
+
+onUnmounted(() => {
+    if (timer.value) {
+        clearInterval(timer.value)
+    }
+    timer.value = null
+})
+
+defineExpose({
+    showModal
+})
+</script>
+  
+<style lang="scss">
+.zqy-log-modal {
+    .modal-content {
+        .content-box {
+            min-height: 60vh;
+            max-height: 60vh;
+            padding: 12px 20px;
+            box-sizing: border-box;
+            overflow: auto;
+
+            pre {
+                color: $--app-base-font-color;
+                font-size: 12px;
+                line-height: 21px;
+                margin: 0;
+            }
+        }
+    }
+}
+</style>
+  
---
+++ spark-yun-website/src/views/workflow/workflow-page/config-detail/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <BlockDrawer :drawer-config="drawerConfig">
+    </BlockDrawer>
+</template>
+
+<script lang="ts" setup>
+import BlockDrawer from '@/components/block-drawer/index.vue'
+import { reactive } from 'vue'
+import { FormInstance, FormRules } from 'element-plus'
+
+// const form = ref<FormInstance>()
+// const callback = ref<any>()
+const drawerConfig = reactive({
+  title: '配置',
+  visible: false,
+  width: '500',
+  okConfig: {
+    title: '确定',
+    ok: okEvent,
+    disabled: false,
+    loading: false
+  },
+  cancelConfig: {
+    title: '取消',
+    cancel: closeEvent,
+    disabled: false
+  },
+  zIndex: 1100,
+  closeOnClickModal: false
+})
+
+function showModal() {
+  drawerConfig.visible = true
+}
+
+function okEvent() {
+  drawerConfig.visible = false
+}
+
+function closeEvent() {
+  drawerConfig.visible = false
+}
+
+defineExpose({
+    showModal
+})
+</script>
