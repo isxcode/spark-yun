4c4982b37b7caf207d7130fe35bbd401fa49cc65
ispong <ispong@outlook.com>
2023-08-20 13:36:21 +0800

    :bug: 修复节点不能支持gitpod访问

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/agent/pojos/req/SparkSubmit.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/agent/pojos/req/SparkSubmit.java
@@ -2,11 +2,15 @@ package com.isxcode.star.api.agent.pojos.req;
 
 import java.util.List;
 import java.util.Map;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SparkSubmit {
 
   private Map<String, String> conf;
