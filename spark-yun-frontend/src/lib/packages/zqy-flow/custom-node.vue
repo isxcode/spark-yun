e076f82172b52c48aec781d4452491baa32e57c6
fancinate <1585546519@qq.com>
2023-08-22 21:33:24 +0800

    :art: 节点上运行成功后展示查看运行日志

--- spark-yun-frontend/src/lib/packages/zqy-flow/custom-node.vue
+++ spark-yun-frontend/src/lib/packages/zqy-flow/custom-node.vue
@@ -15,7 +15,8 @@
                 <template #dropdown>
                     <el-dropdown-menu>
                         <el-dropdown-item command="node_log">日志</el-dropdown-item>
-                        <el-dropdown-item command="node_result">运行结果</el-dropdown-item>
+                        <el-dropdown-item v-if="status !== 'RUNNING' && node.data.nodeConfigData.workType !== 'EXE_JDBC'" command="node_result">运行结果</el-dropdown-item>
+                        <el-dropdown-item v-if="status !== 'RUNNING' && node.data.nodeConfigData.workType === 'SPARK_SQL'" command="node_yarnLog">运行日志</el-dropdown-item>
                         <el-dropdown-item command="node_runAfter">重跑下游</el-dropdown-item>
                         <el-dropdown-item command="node_break">中断</el-dropdown-item>
                         <el-dropdown-item command="node_reRun">重跑当前</el-dropdown-item>
