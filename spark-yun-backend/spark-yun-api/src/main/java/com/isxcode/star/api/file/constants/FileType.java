544cf38e8452dee3e8005f829d29d4fd22892c58
wq <2126524086@qq.com>
2023-09-12 11:33:15 +0800

    :sparkles: 新增文件模块。

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/file/constants/FileType.java
@@ -0,0 +1,11 @@
+package com.isxcode.star.api.file.constants;
+
+/** 作业类型. */
+public interface FileType {
+
+  String SPARK_JOB = "sparkJob";
+
+  String LIB = "lib";
+
+  String DRIVER = "driver";
+}
--- spark-yun-backend/spark-yun-main/src/main/resources/db/migration/h2/V0.2.0__#159.sql
+++ spark-yun-backend/spark-yun-main/src/main/resources/db/migration/h2/V0.2.0__#159.sql
@@ -22,3 +22,23 @@ create table SY_SYNC_WORK_CONFIG
   tenant_id               varchar(200)  not null comment '租户id'
 );
 CREATE UNIQUE INDEX SY_SYNC_WORK_CONFIG_work_id_IDX USING BTREE ON zhiqingyun.SY_SYNC_WORK_CONFIG (work_id);
+
+-- 创建资源文件表
+create table SY_FILE
+(
+  id                      varchar(200)  not null comment '文件配置唯一id'
+    primary key,
+  file_name                varchar(200)  null comment '文件名称',
+  file_size                varchar(200)  null comment '文件大小',
+  file_path                varchar(200)  null comment '文件存储路径',
+  file_type                varchar(200)  null comment '文件类型',
+  create_by               varchar(200)  not null comment '创建人',
+  create_date_time        datetime      not null comment '创建时间',
+  last_modified_by        varchar(200)  not null comment '更新人',
+  last_modified_date_time datetime      not null comment '更新时间',
+  version_number          int           not null comment '版本号',
+  deleted                 int default 0 not null comment '逻辑删除',
+  tenant_id               varchar(200)  not null comment '租户id',
+  constraint id
+    unique (id)
+);
