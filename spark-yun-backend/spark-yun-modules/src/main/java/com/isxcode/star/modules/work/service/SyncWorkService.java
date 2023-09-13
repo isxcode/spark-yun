3e4afd901444a10a50bf3e407f463e28101438cf
wq <2126524086@qq.com>
2023-09-13 15:29:10 +0800

    :sparkles: 新增数据预览与ddl预生成。

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/service/SyncWorkService.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/service/SyncWorkService.java
@@ -1,21 +1,31 @@
 package com.isxcode.star.modules.work.service;
 
+import com.isxcode.star.backend.api.base.exceptions.IsxAppException;
+import com.isxcode.star.common.connection.JDBCConnection;
+import com.isxcode.star.common.utils.AesUtils;
+import com.isxcode.star.modules.datasource.entity.DatasourceEntity;
+import com.isxcode.star.modules.datasource.repository.DatasourceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static com.isxcode.star.api.datasource.constants.DatasourceType.ORACLE;
+import static com.isxcode.star.api.datasource.constants.DatasourceType.SQL_SERVER;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class SyncWorkService {
+
+  private final DatasourceRepository datasourceRepository;
+
+  private final AesUtils aesUtils;
 
   public List<String> tables(DatabaseMetaData metaData, String catalog, String schema) throws SQLException {
     List<String> list = new ArrayList<>();
@@ -45,7 +55,7 @@ public class SyncWorkService {
       List<String> cList = new ArrayList<>();
       cList.add(columns.getString("COLUMN_NAME"));
       cList.add(columns.getString("TYPE_NAME"));
-      //cList.add(columns.getInt("COLUMN_SIZE"));
+      cList.add(String.valueOf(columns.getInt("COLUMN_SIZE")));
       list.add(cList);
     }
     return list;
@@ -69,5 +79,28 @@ public class SyncWorkService {
     result.put("schema", schema);
 
     return result;
+  }
+
+  public Connection getConnection(String dataSourceId, String driver, String classPath) throws Exception {
+    Optional<DatasourceEntity> datasourceEntityOptional = datasourceRepository.findById(dataSourceId);
+    if (!datasourceEntityOptional.isPresent()) {
+      throw new IsxAppException("数据源异常，请联系开发者");
+    }
+
+    return JDBCConnection.getConnection(datasourceEntityOptional.get().getJdbcUrl(),
+      datasourceEntityOptional.get().getUsername(),
+      aesUtils.decrypt(datasourceEntityOptional.get().getPasswd()),
+      driver, classPath);
+  }
+
+  public String getDataPreviewSql(String dataType, String tableName) {
+    switch (dataType){
+      case ORACLE:
+        return "SELECT * FROM "+ tableName + " WHERE ROWNUM <= 50";
+      case SQL_SERVER:
+        return "SELECT TOP 50 * FROM "+ tableName;
+      default:
+        return "SELECT * FROM "+ tableName + " LIMIT 50";
+    }
   }
 }
