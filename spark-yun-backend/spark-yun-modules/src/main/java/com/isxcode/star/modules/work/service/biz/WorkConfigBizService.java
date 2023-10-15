ec717f230288a722090ec30e3c0fe9d43ea66aa4
ispong <ispong@outlook.com>
2023-10-15 11:33:30 +0800

    :bug: 修复sparkconfig的json识别问题

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/service/biz/WorkConfigBizService.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/service/biz/WorkConfigBizService.java
@@ -7,82 +7,86 @@ import com.isxcode.star.modules.work.entity.WorkConfigEntity;
 import com.isxcode.star.modules.work.entity.WorkEntity;
 import com.isxcode.star.modules.work.repository.WorkConfigRepository;
 import com.isxcode.star.modules.work.repository.WorkRepository;
+
 import java.util.Optional;
 import javax.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.scheduling.support.CronExpression;
 import org.springframework.stereotype.Service;
 
-/** 用户模块接口的业务逻辑. */
+/**
+ * 用户模块接口的业务逻辑.
+ */
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
 public class WorkConfigBizService {
 
-	private final WorkRepository workRepository;
+  private final WorkRepository workRepository;
 
-	private final WorkConfigRepository workConfigRepository;
+  private final WorkConfigRepository workConfigRepository;
 
-	public WorkConfigEntity getWorkConfigEntity(String workConfigId) {
+  public WorkConfigEntity getWorkConfigEntity(String workConfigId) {
 
-		Optional<WorkConfigEntity> workConfigEntityOptional = workConfigRepository.findById(workConfigId);
-		if (!workConfigEntityOptional.isPresent()) {
-			throw new IsxAppException("作业异常，请联系开发者");
-		}
-		return workConfigEntityOptional.get();
-	}
+    Optional<WorkConfigEntity> workConfigEntityOptional = workConfigRepository.findById(workConfigId);
+    if (!workConfigEntityOptional.isPresent()) {
+      throw new IsxAppException("作业异常，请联系开发者");
+    }
+    return workConfigEntityOptional.get();
+  }
 
-	public void configWork(ConfigWorkReq wocConfigWorkReq) {
+  public void configWork(ConfigWorkReq wocConfigWorkReq) {
 
-		// 校验sparkConfig为json格式
-		if (!JSON.isValid(wocConfigWorkReq.getSparkConfig())) {
-			throw new IsxAppException("500", "sparkConfig中json格式不合法");
-		}
+    // 校验sparkConfig为json格式
+    if (!Strings.isEmpty(wocConfigWorkReq.getSparkConfig()) && !JSON.isValid(wocConfigWorkReq.getSparkConfig())) {
+      throw new IsxAppException("500", "sparkConfig中json格式不合法");
+    }
 
-		Optional<WorkEntity> workEntityOptional = workRepository.findById(wocConfigWorkReq.getWorkId());
-		if (!workEntityOptional.isPresent()) {
-			throw new IsxAppException("作业不存在");
-		}
+    Optional<WorkEntity> workEntityOptional = workRepository.findById(wocConfigWorkReq.getWorkId());
+    if (!workEntityOptional.isPresent()) {
+      throw new IsxAppException("作业不存在");
+    }
 
-		Optional<WorkConfigEntity> workConfigEntityOptional = workConfigRepository
-				.findById(workEntityOptional.get().getConfigId());
-		if (!workConfigEntityOptional.isPresent()) {
-			throw new IsxAppException("作业异常，作业不可用。");
-		}
-		WorkConfigEntity workConfigEntity = workConfigEntityOptional.get();
+    Optional<WorkConfigEntity> workConfigEntityOptional = workConfigRepository
+      .findById(workEntityOptional.get().getConfigId());
+    if (!workConfigEntityOptional.isPresent()) {
+      throw new IsxAppException("作业异常，作业不可用。");
+    }
+    WorkConfigEntity workConfigEntity = workConfigEntityOptional.get();
 
-		if (!Strings.isEmpty(wocConfigWorkReq.getSqlScript())) {
-			workConfigEntity.setSqlScript(wocConfigWorkReq.getSqlScript());
-		}
-		if (!Strings.isEmpty(wocConfigWorkReq.getClusterId())) {
-			workConfigEntity.setClusterId(wocConfigWorkReq.getClusterId());
-		}
-		if (!Strings.isEmpty(wocConfigWorkReq.getClusterNodeId())) {
-			workConfigEntity.setClusterNodeId(wocConfigWorkReq.getClusterNodeId());
-		}
-		if (!Strings.isEmpty(wocConfigWorkReq.getDatasourceId())) {
-			workConfigEntity.setDatasourceId(wocConfigWorkReq.getDatasourceId());
-		}
-		if (!Strings.isEmpty(wocConfigWorkReq.getSparkConfig())) {
-			workConfigEntity.setSparkConfig(wocConfigWorkReq.getSparkConfig());
-		}
-		if (!Strings.isEmpty(wocConfigWorkReq.getSyncConf())) {
-			workConfigEntity.setSyncConf(wocConfigWorkReq.getSyncConf());
-		}
-		if (!Strings.isEmpty(wocConfigWorkReq.getBashScript())) {
-			workConfigEntity.setBashScript(wocConfigWorkReq.getBashScript());
-		}
-		if (!Strings.isEmpty(wocConfigWorkReq.getCorn())) {
-			// 检验corn表达式
-			boolean validExpression = CronExpression.isValidExpression(wocConfigWorkReq.getCorn());
-			if (!validExpression) {
-				throw new IsxAppException("Corn表达式异常");
-			}
-			workConfigEntity.setCorn(wocConfigWorkReq.getCorn());
-		}
-		workConfigRepository.save(workConfigEntity);
-	}
+    if (!Strings.isEmpty(wocConfigWorkReq.getSqlScript())) {
+      workConfigEntity.setSqlScript(wocConfigWorkReq.getSqlScript());
+    }
+    if (!Strings.isEmpty(wocConfigWorkReq.getClusterId())) {
+      workConfigEntity.setClusterId(wocConfigWorkReq.getClusterId());
+    }
+    if (!Strings.isEmpty(wocConfigWorkReq.getClusterNodeId())) {
+      workConfigEntity.setClusterNodeId(wocConfigWorkReq.getClusterNodeId());
+    }
+    if (!Strings.isEmpty(wocConfigWorkReq.getDatasourceId())) {
+      workConfigEntity.setDatasourceId(wocConfigWorkReq.getDatasourceId());
+    }
+    if (!Strings.isEmpty(wocConfigWorkReq.getSparkConfig())) {
+      workConfigEntity.setSparkConfig(wocConfigWorkReq.getSparkConfig());
+    }
+    if (!Strings.isEmpty(wocConfigWorkReq.getSyncConf())) {
+      workConfigEntity.setSyncConf(wocConfigWorkReq.getSyncConf());
+    }
+    if (!Strings.isEmpty(wocConfigWorkReq.getBashScript())) {
+      workConfigEntity.setBashScript(wocConfigWorkReq.getBashScript());
+    }
+    if (!Strings.isEmpty(wocConfigWorkReq.getCorn())) {
+      // 检验corn表达式
+      boolean validExpression = CronExpression.isValidExpression(wocConfigWorkReq.getCorn());
+      if (!validExpression) {
+        throw new IsxAppException("Corn表达式异常");
+      }
+      workConfigEntity.setCorn(wocConfigWorkReq.getCorn());
+    }
+    workConfigRepository.save(workConfigEntity);
+  }
 }
