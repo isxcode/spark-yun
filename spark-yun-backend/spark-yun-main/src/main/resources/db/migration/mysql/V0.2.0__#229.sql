ff96c23d1aeadc92f73383c36ab847b21b296a3e
ispong <ispong@outlook.com>
2023-10-15 09:16:15 +0800

    :bug: 修复合并完代码无法启动问题

--- spark-yun-backend/spark-yun-main/src/main/resources/db/migration/mysql/V0.2.0__#229.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE SY_WORK_CONFIG ADD bash_script text NULL;
-ALTER TABLE SY_WORK_CONFIG CHANGE bash_script bash_script text NULL AFTER sql_script;
-
-ALTER TABLE SY_WORK_CONFIG ADD cluster_node_id varchar(200) NULL;
-ALTER TABLE SY_WORK_CONFIG CHANGE cluster_node_id cluster_node_id varchar(200) NULL AFTER cluster_id;
-
