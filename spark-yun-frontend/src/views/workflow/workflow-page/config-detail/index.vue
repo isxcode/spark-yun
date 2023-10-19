f26ec7d8f7cfde5ac3744477c11d771327423e29
fancinate <1585546519@qq.com>
2023-10-19 21:22:29 +0800

    :zap: 数据同步配置抽屉开发

--- spark-yun-frontend/src/views/workflow/workflow-page/config-detail/index.vue
+++ spark-yun-frontend/src/views/workflow/workflow-page/config-detail/index.vue
@@ -1,165 +1,252 @@
 <template>
   <BlockDrawer :drawer-config="drawerConfig">
-    <!-- <span style="font-size: 12px">表达式：{{ cron }}</span> -->
-    <el-form
-      ref="form"
-      class="config-detail-form"
-      label-position="left"
-      :model="formData"
-      label-width="140px"
-    >
-      <el-form-item label="调度状态">
-        <el-switch :disabled="false" v-model="formData.scheduleStatus" />
-      </el-form-item>
-      <el-form-item label="生效时间">
-        <el-date-picker
-          v-model="formData.workDateRange"
-          :disabled="!formData.scheduleStatus"
-          type="daterange"
-          start-placeholder="开始时间"
-          end-placeholder="结束时间"
-        />
-      </el-form-item>
-
-      <el-form-item label="配置方式">
-        <el-radio-group v-model="formData.cronType" :disabled="!formData.scheduleStatus">
-          <el-radio :label="'0'">手动配置</el-radio>
-          <el-radio :label="'1'">自定义配置</el-radio>
-        </el-radio-group>
-      </el-form-item>
-
-      <template v-if="formData.cronType === '1'">
-        <el-form-item label="Corn表达式">
-          <el-input :disabled="!formData.scheduleStatus" v-model="formData.corn" placeholder="请输入" show-word-limit />
-        </el-form-item>
-      </template>
-      <template v-if="formData.cronType === '0'">
-        <el-form-item label="调度周期">
-          <el-select v-model="formData.range" placeholder="请选择" :disabled="!formData.scheduleStatus" @change="changeScheduleRangeEvent">
-            <el-option
-              v-for="item in scheduleRange"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            />
-          </el-select>
-        </el-form-item>
-        <!-- 调度周期 -> 秒 -->
-        <template v-if="formData.range === 'sec'">
-          <el-form-item label="结束时间">
-            <el-date-picker :disabled="!formData.scheduleStatus" v-model="formData.endDate" type="date" placeholder="请选择" clearable />
-          </el-form-item>
-        </template>
-        <!-- 调度周期 -> 分钟 -->
-        <template v-if="formData.range === 'min'">
-          <el-form-item label="开始时间">
-            <el-time-select
-              v-model="formData.startDateMin"
-              :disabled="!formData.scheduleStatus"
-              start="00:00"
-              step="01:00"
-              end="23:00"
-              placeholder="请选择">
-            </el-time-select>
-          </el-form-item>
-          <el-form-item label="时间间隔（分钟）">
-            <el-input-number :disabled="!formData.scheduleStatus" v-model="formData.minNum" :min="0" controls-position="right" />
-          </el-form-item>
-          <el-form-item label="结束时间">
-            <el-time-select
-              v-model="formData.endDateMin"
-              :disabled="!formData.scheduleStatus"
-              start="00:00"
-              step="01:00"
-              end="23:00"
-              placeholder="请选择">
-            </el-time-select>
-          </el-form-item>
-        </template>
-        <!-- 调度周期 -> 小时 -->
-        <template v-if="formData.range === 'hour'">
-          <el-form-item label="开始时间">
-            <el-time-select
-              :disabled="!formData.scheduleStatus"
-              v-model="formData.startDate"
-              start="00:00"
-              step="01:00"
-              end="23:00"
-              placeholder="请选择">
-            </el-time-select>
-          </el-form-item>
-          <el-form-item label="时间间隔（小时）">
-            <el-input-number :disabled="!formData.scheduleStatus" v-model="formData.hourNum" :min="0" controls-position="right" />
-          </el-form-item>
-          <el-form-item label="结束时间">
-            <el-time-select
-              v-model="formData.endDate"
-              :disabled="!formData.scheduleStatus"
-              start="00:00"
-              step="01:00"
-              end="23:00"
-              placeholder="请选择">
-            </el-time-select>
-          </el-form-item>
-        </template>
-        <!-- 调度周期 -> 日 -->
-        <template v-if="formData.range === 'day'">
-          <el-form-item label="调度时间">
-            <el-time-picker
-              :disabled="!formData.scheduleStatus"
-              v-model="formData.scheduleDate"
-              format="hh:mm"
-              value-format="hh:mm"
-              placeholder="请选择"
-            />
-          </el-form-item>
-        </template>
-        <!-- 调度周期 -> 月 -->
-        <template v-if="formData.range === 'month'">
-          <el-form-item label="调度时间">
-            <el-time-picker
-              :disabled="!formData.scheduleStatus"
-              v-model="formData.scheduleDate"
-              format="hh:mm"
-              value-format="hh:mm"
-              placeholder="请选择"
-            />
-          </el-form-item>
-          <el-form-item label="指定时间">
-            <el-select v-model="formData.monthDay" :disabled="!formData.scheduleStatus" placeholder="请选择">
-              <el-option
-                v-for="item in dayList"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              />
-            </el-select>
-          </el-form-item>
-        </template>
-        <!-- 调度周期 -> 周 -->
-        <template v-if="formData.range === 'week'">
-          <el-form-item label="调度时间">
-            <el-time-picker
-              :disabled="!formData.scheduleStatus"
-              v-model="formData.scheduleDate"
-              format="hh:mm"
-              value-format="hh:mm"
-              placeholder="请选择"
-            />
-          </el-form-item>
-          <el-form-item label="指定时间">
-            <el-select v-model="formData.weekDate" placeholder="请选择" :disabled="!formData.scheduleStatus">
-              <el-option
-                v-for="item in weekDateList"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              />
-            </el-select>
-          </el-form-item>
-        </template>
-      </template>
-    </el-form>
+    <el-scrollbar>
+        <div class="work-flow-config">
+          <!-- 资源配置 -->
+          <div class="config-item">
+            <div class="item-title">资源配置</div>
+            <el-form
+              ref="form"
+              label-position="left"
+              label-width="120px"
+              :model="formData"
+              :rules="rules"
+            >
+              <el-form-item label="模式">
+                <el-radio-group v-model="formData.type" size="small">
+                  <el-radio-button label="0">简易</el-radio-button>
+                  <el-radio-button label="1">高级定义</el-radio-button>
+                </el-radio-group>
+              </el-form-item>
+              <el-form-item label="计算集群">
+                <el-select v-model="formData.sourceLevel" placeholder="请选择">
+                  <el-option
+                    v-for="item in sourceLevelOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="sparkConfig" v-if="formData.type === '1'">
+                <el-input
+                  v-model="formData.sparkConfig"
+                  :rows="6"
+                  type="textarea"
+                  placeholder="请输入"
+                />
+              </el-form-item>
+              <el-form-item label="资源等级" v-else>
+                <el-select v-model="formData.sourceLevel" placeholder="请选择">
+                  <el-option
+                    v-for="item in sourceLevelOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-form>
+          </div>
+          <el-divider />
+          <!-- 定时调度 -->
+          <div class="config-item">
+            <div class="item-title">调度配置</div>
+            <el-form
+              ref="form"
+              label-position="left"
+              label-width="120px"
+              :model="formData"
+              :rules="rules"
+            >
+              <el-form-item label="启用">
+                <el-switch v-model="formData.cronType" />
+              </el-form-item>
+              <el-form-item label="模式">
+                <el-radio-group v-model="formData.cronType" size="small">
+                  <el-radio-button label="0">简易</el-radio-button>
+                  <el-radio-button label="1">高级定义</el-radio-button>
+                </el-radio-group>
+              </el-form-item>
+              <el-form-item label="cron表达式" v-if="formData.cronType === '1'">
+                <el-input
+                  v-model="formData.sparkConfig"
+                  placeholder="请输入"
+                />
+              </el-form-item>
+              <template v-else>
+                <el-form-item label="调度周期">
+                  <el-select v-model="formData.range" placeholder="请选择" :disabled="!formData.scheduleStatus" @change="changeScheduleRangeEvent">
+                    <el-option
+                      v-for="item in scheduleRange"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    />
+                  </el-select>
+                </el-form-item>
+                <!-- 调度周期 -> 秒 -->
+                <template v-if="formData.range === 'sec'">
+                  <el-form-item label="结束时间">
+                    <el-date-picker :disabled="!formData.scheduleStatus" v-model="formData.endDate" type="date" placeholder="请选择" clearable />
+                  </el-form-item>
+                </template>
+                <!-- 调度周期 -> 分钟 -->
+                <template v-if="formData.range === 'min'">
+                  <el-form-item label="开始时间">
+                    <el-time-select
+                      v-model="formData.startDateMin"
+                      :disabled="!formData.scheduleStatus"
+                      start="00:00"
+                      step="01:00"
+                      end="23:00"
+                      placeholder="请选择">
+                    </el-time-select>
+                  </el-form-item>
+                  <el-form-item label="时间间隔（分钟）">
+                    <el-input-number :disabled="!formData.scheduleStatus" v-model="formData.minNum" :min="0" controls-position="right" />
+                  </el-form-item>
+                  <el-form-item label="结束时间">
+                    <el-time-select
+                      v-model="formData.endDateMin"
+                      :disabled="!formData.scheduleStatus"
+                      start="00:00"
+                      step="01:00"
+                      end="23:00"
+                      placeholder="请选择">
+                    </el-time-select>
+                  </el-form-item>
+                </template>
+                <!-- 调度周期 -> 小时 -->
+                <template v-if="formData.range === 'hour'">
+                  <el-form-item label="开始时间">
+                    <el-time-select
+                      :disabled="!formData.scheduleStatus"
+                      v-model="formData.startDate"
+                      start="00:00"
+                      step="01:00"
+                      end="23:00"
+                      placeholder="请选择">
+                    </el-time-select>
+                  </el-form-item>
+                  <el-form-item label="时间间隔（小时）">
+                    <el-input-number :disabled="!formData.scheduleStatus" v-model="formData.hourNum" :min="0" controls-position="right" />
+                  </el-form-item>
+                  <el-form-item label="结束时间">
+                    <el-time-select
+                      v-model="formData.endDate"
+                      :disabled="!formData.scheduleStatus"
+                      start="00:00"
+                      step="01:00"
+                      end="23:00"
+                      placeholder="请选择">
+                    </el-time-select>
+                  </el-form-item>
+                </template>
+                <!-- 调度周期 -> 日 -->
+                <template v-if="formData.range === 'day'">
+                  <el-form-item label="调度时间">
+                    <el-time-picker
+                      :disabled="!formData.scheduleStatus"
+                      v-model="formData.scheduleDate"
+                      format="hh:mm"
+                      value-format="hh:mm"
+                      placeholder="请选择"
+                    />
+                  </el-form-item>
+                </template>
+                <!-- 调度周期 -> 月 -->
+                <template v-if="formData.range === 'month'">
+                  <el-form-item label="调度时间">
+                    <el-time-picker
+                      :disabled="!formData.scheduleStatus"
+                      v-model="formData.scheduleDate"
+                      format="hh:mm"
+                      value-format="hh:mm"
+                      placeholder="请选择"
+                    />
+                  </el-form-item>
+                  <el-form-item label="指定时间">
+                    <el-select v-model="formData.monthDay" :disabled="!formData.scheduleStatus" placeholder="请选择">
+                      <el-option
+                        v-for="item in dayList"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                      />
+                    </el-select>
+                  </el-form-item>
+                </template>
+                <!-- 调度周期 -> 周 -->
+                <template v-if="formData.range === 'week'">
+                  <el-form-item label="调度时间">
+                    <el-time-picker
+                      :disabled="!formData.scheduleStatus"
+                      v-model="formData.scheduleDate"
+                      format="hh:mm"
+                      value-format="hh:mm"
+                      placeholder="请选择"
+                    />
+                  </el-form-item>
+                  <el-form-item label="指定时间">
+                    <el-select v-model="formData.weekDate" placeholder="请选择" :disabled="!formData.scheduleStatus">
+                      <el-option
+                        v-for="item in weekDateList"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                      />
+                    </el-select>
+                  </el-form-item>
+                </template>
+              </template>
+            </el-form>
+          </div>
+          <el-divider />
+          <!-- 定时调度 -->
+          <div class="config-item">
+            <div class="item-title">同步规则</div>
+            <el-form
+              ref="form"
+              label-position="left"
+              label-width="120px"
+              :model="formData"
+              :rules="rules"
+            >
+              <el-form-item label="模式">
+                <el-radio-group v-model="formData.cronType" size="small">
+                  <el-radio-button label="0">简易</el-radio-button>
+                  <el-radio-button label="1">高级定义</el-radio-button>
+                </el-radio-group>
+              </el-form-item>
+              <el-form-item label="sparkConfig" v-if="formData.cronType === '1'">
+                <el-input
+                  v-model="formData.sparkConfig"
+                  placeholder="请输入"
+                />
+              </el-form-item>
+              <template v-else>
+                <el-form-item label="分区数">
+                  <el-input-number
+                    v-model="num"
+                    :min="0"
+                    placeholder="请输入"
+                    controls-position="right"
+                  />
+                </el-form-item>
+                <el-form-item label="并发数">
+                  <el-input-number
+                    v-model="num"
+                    :min="0"
+                    placeholder="请输入"
+                    controls-position="right"
+                  />
+                </el-form-item>
+              </template>
+            </el-form>
+          </div>
+        </div>
+      </el-scrollbar>
   </BlockDrawer>
 </template>
 
@@ -167,62 +254,13 @@
 import { computed, reactive, ref } from 'vue'
 import { FormInstance, FormRules } from 'element-plus'
 import BlockDrawer from '@/components/block-drawer/index.vue'
+import {ScheduleRange, WeekDateList} from './config-detail'
 
 // const form = ref<FormInstance>()
 const callback = ref()
 
-const scheduleRange = ref([
-  {
-    label: '分钟',
-    value: 'min',
-  },
-  {
-    label: '小时',
-    value: 'hour',
-  },
-  {
-    label: '日',
-    value: 'day',
-  },
-  {
-    label: '月',
-    value: 'month',
-  },
-  {
-    label: '星期',
-    value: 'week',
-  }
-]);
-const weekDateList = ref([
-  {
-    label: '星期一',
-    value: '1',
-  },
-  {
-    label: '星期二',
-    value: '2',
-  },
-  {
-    label: '星期三',
-    value: '3',
-  },
-  {
-    label: '星期四',
-    value: '4',
-  },
-  {
-    label: '星期五',
-    value: '5',
-  },
-  {
-    label: '星期六',
-    value: '6',
-  },
-  {
-    label: '星期日',
-    value: '7',
-  }
-])
+const scheduleRange = ref(ScheduleRange);
+const weekDateList = ref(WeekDateList)
 const dayList = ref()
 const drawerConfig = reactive({
   title: '配置',
@@ -248,19 +286,14 @@ const formData = reactive({
   cronType: '',       // 配置类型    
   corn: '',
   range: '', // 调度周期
-
   startDateMin: '',  // 开始时间 - 分钟
   minNum: '',        // 间隔时间 - 分钟
   endDateMin: '',    // 结束时间 - 分钟
-
   startDate: '',     // 开始时间 - 小时
   hourNum: null,     // 间隔时间 - 小时
   endDate: '',       // 结束时间 - 小时
-
   scheduleDate: '',  // 调度时间 - 日/周
-
   weekDate: '',      // 指定时间 - 星期
-
   monthDay: '',      // 指定时间 - 月
 
 });
@@ -346,22 +379,39 @@ function getCron() {
     state.daysText = '?'
     state.weeksText = `${formData.weekDate}`
   }
-
-  // state.secondsText = ''
-  // state.minutesText = '*'
-  // state.hoursText = '*'
-  // state.daysText = '*'
-  // state.weeksText = '?'
-  // state.monthsText = '*'
-  // state.yearsText = '*'
 }
 
 defineExpose({
   showModal,
-});
+})
 </script>
 
 <style lang="scss">
+.work-flow-config {
+  padding: 12px;
+  .config-item {
+    .item-title {
+      font-size: 12px;
+      padding-bottom: 12px;
+      box-sizing: border-box;
+      border-bottom: 1px solid #ebeef5;
+    }
+    .el-form {
+      padding: 12px 0 0;
+      box-sizing: border-box;
+      .el-form-item {
+        .el-form-item__content {
+          .el-radio-group {
+            justify-content: flex-end;
+          }
+        }
+      }
+    }
+  }
+  .el-divider {
+    margin: 0 0 12px;
+  }
+}
 .config-detail-form {
   padding: 12px 20px 0 20px;
   box-sizing: border-box;
