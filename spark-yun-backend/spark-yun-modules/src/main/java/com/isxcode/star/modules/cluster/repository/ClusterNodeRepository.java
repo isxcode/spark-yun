0141f4909567ba9b1e70131db83bce9ab62ecef1
ispong <ispong@outlook.com>
2023-10-30 15:39:25 +0800

    :sparkles: 支持监控安装进度

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/cluster/repository/ClusterNodeRepository.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/cluster/repository/ClusterNodeRepository.java
@@ -1,6 +1,7 @@
 package com.isxcode.star.modules.cluster.repository;
 
 import com.isxcode.star.modules.cluster.entity.ClusterNodeEntity;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -8,11 +9,14 @@ import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-/** 只负责数据库查询逻辑. */
+/**
+ * 只负责数据库查询逻辑.
+ */
 @Repository
 @CacheConfig(cacheNames = {"SY_ENGINE_NODE"})
 public interface ClusterNodeRepository extends JpaRepository<ClusterNodeEntity, String> {
@@ -27,4 +31,9 @@ public interface ClusterNodeRepository extends JpaRepository<ClusterNodeEntity, 
 			+ "OR E.remark LIKE %:keyword% " + "OR E.host LIKE %:keyword%) order by E.createDateTime desc ")
 	Page<ClusterNodeEntity> searchAll(@Param("keyword") String searchKeyWord, @Param("engineId") String engineId,
 			Pageable pageable);
+
+	@Modifying
+	@Query(value = "update SY_CLUSTER_NODE set agentLog = :agentLog where id = :id", nativeQuery = true)
+	void updateClusterNodeAgentLog(@Param("id") String id, @Param("agentLog") String agentLog);
+
 }
