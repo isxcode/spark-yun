5449b81d483d79698443de66c77f36f13fc572fd
aoyia <>
2023-11-04 02:37:47 +0800

    :memo: 官网文档重构nuxtContent

--- docs/components/layout/home-header.vue
+++ docs/components/layout/home-header.vue
@@ -4,27 +4,34 @@
       <div class="right">
         <div @click="handleLogoClick" class="home-header-logo">
           <div class="logo">
-            <img src="~assets/images/favicon.png" alt="" />
+            <img src="~assets/images/logo.png" alt="" />
             <h1>至轻云</h1>
           </div>
         </div>
       </div>
       <div class="center">
         <div class="menu">
-          <div class="menu-item" v-for="(item, index) in menuData" @click="handleMenuClick(item.path)" :key="index">
-            {{ item.title }}
+          <div
+            class="menu-item"
+            v-for="(item, index) in menuData"
+            @click="handleMenuClick(item)"
+            :key="index"
+          >
+            <UIcon v-if="item.icon" :name="item.icon" class="icon" />
+            <span v-if="!item.icon">
+              {{ item.title }}
+            </span>
           </div>
         </div>
       </div>
       <div class="left">
-        <div class="quick-use">快速使用</div>
+        <div class="quick-use" @click="handleQuickUseClick">快速试用</div>
       </div>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
-
 defineComponent("LayoutHomeHeader");
 const headerRef = ref<HTMLElement | null>(null);
 
@@ -33,15 +40,11 @@ onMounted(() => {
 });
 
 function handleScroll() {
-  const blackList = ["/docs", "/blog"];
-  const flag = blackList.some((item) => {
-    // 路径域名之后的内容
-    const path = window.location.pathname.split("/")[1];
-    return item === `/${path}`;
-  });
-  if (flag) {
+  const flag = window.location.pathname === "/";
+  if (!flag) {
     headerRef.value!.style.backgroundColor = "var(--sk-color-home-bgc)";
     headerRef.value!.style.height = "60px";
+    headerRef.value!.style.boxShadow = "0 2px 4px -1px rgba(0,0,0,0.25)";
     return;
   }
   if (window.scrollY > 0) {
@@ -54,23 +57,20 @@ function handleScroll() {
   }
 }
 
-// 监听路由变化，当路由为 /docs 或者 /blog 时，header 的背景色为白色，高度固定为 60px
 const router = useRouter();
 
 watch(
   () => router.currentRoute.value.path,
   (path) => {
-    const blackList = ["/docs", "/blog"];
-    const flag = blackList.some((item) => {
-      // 路径域名之后的内容
+    const whiteList = ["/"];
+    const flag = whiteList.some((item) => {
       const path = window.location.pathname.split("/")[1];
       return item === `/${path}`;
     });
-    if (flag) {
+    if (!flag) {
       headerRef.value!.style.backgroundColor = "var(--sk-color-home-bgc)";
       headerRef.value!.style.height = "60px";
       headerRef.value!.style.boxShadow = "0 2px 4px -1px rgba(0,0,0,0.25)";
-
       return;
     }
     if (path === "/") {
@@ -86,25 +86,48 @@ function handleLogoClick() {
   router.push("/");
 }
 
-const menuData = reactive([
+// 菜单数据接口interface
+
+interface MenuData {
+  title: string;
+  path: string;
+  type: string;
+  icon?: string;
+}
+
+const menuData: Array<MenuData> = reactive([
   {
-    title: "文档",
-    path: "/docs",
-  },
-  {
-    title: "博客",
-    path: "/blog",
+    title: "文档中心",
+    path: "/install/docker-deploy",
+    type: "router",
   },
   {
     title: "github",
-    path: "",
+    icon: "i-simple-icons-github",
+    path: "https://github.com/isxcode/spark-yun",
+    type: "link",
+  },
+  {
+    title: "gitee",
+    icon: "i-simple-icons-gitee",
+    path: "https://gitee.com/isxcode/spark-yun",
+    type: "link",
   },
 ]);
-function handleMenuClick(path: string) {
-  const router = useRouter();
-  if (path) {
-    router.push(path);
+function handleMenuClick(menuItem: MenuData) {
+  if (menuItem.type === "router") {
+    const router = useRouter();
+    if (menuItem.path) {
+      router.push(menuItem.path);
+    }
   }
+  if (menuItem.type === "link") {
+    window.open(menuItem.path);
+  }
+}
+
+function handleQuickUseClick() {
+  window.open("https://zhiqingyun-demo.isxcode.com");
 }
 </script>
 
@@ -121,45 +144,38 @@ function handleMenuClick(path: string) {
   height: 80px;
   display: flex;
   align-items: center;
-
   .content {
-    width: 1348px;
+    width: 1220px;
     margin: 0 auto;
     display: flex;
     align-items: center;
     justify-content: space-between;
-
     .right {
       flex: 0;
       flex-basis: 220px;
     }
-
     .center {
       flex: 1;
     }
-
     .left {
       flex: 0;
       flex-basis: auto;
     }
-
     .home-header-logo {
-      // 鼠标样式
       cursor: pointer;
-      // 防止被遮挡
       z-index: 999;
       .logo {
+        color: var(--sk-color-home-primary);
         width: 154px;
         height: 54px;
         display: flex;
         flex-direction: row;
         align-items: center;
-
         > img {
           width: 32px;
           height: 32px;
+          margin-bottom: 2px;
         }
-
         h1 {
           padding-left: 8px;
           font-size: 24px;
@@ -168,32 +184,35 @@ function handleMenuClick(path: string) {
         }
       }
     }
-
     .menu {
       flex: 1;
       display: flex;
       justify-content: flex-end;
 
       .menu-item {
-        width: 80px;
-        height: 36px;
-        line-height: 36px;
+        padding: 0px 8px;
+        height: 32px;
+        line-height: 32px;
         text-align: center;
         border-radius: 20px;
-        margin-left: 14px;
+        margin-left: 6px;
         cursor: pointer;
         transition: all 0.3s;
         color: var(--sk-color-font-gray);
-
         &:hover {
           color: var(--sk-color-font-gray-hover);
+        }
+
+        > .icon {
+          margin-top: 8px;
         }
       }
     }
     .quick-use {
-      width: 100px;
-      height: 40px;
-      line-height: 40px;
+      width: auto;
+      padding: 0 12px;
+      height: 32px;
+      line-height: 32px;
       text-align: center;
       border-radius: 20px;
       margin-left: 14px;
@@ -201,7 +220,6 @@ function handleMenuClick(path: string) {
       transition: all 0.3s;
       color: var(--sk-color-font-gray);
       border: 1px solid var(--sk-color-font-gray);
-
       &:hover {
         color: var(--sk-color-font-gray-hover);
         border: 1px solid var(--sk-color-font-gray-hover);
--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,15 +1,119 @@
 export default defineNuxtConfig({
+  extends: "@nuxt/ui-pro",
   devtools: { enabled: true },
+  routeRules: {
+    // content.nuxtjs.org redirects
+    "/get-started": { redirect: "/get-started/installation" },
+    "/guide/writing/content-directory": {
+      redirect: "/usage/content-directory",
+    },
+    "/guide/writing/markdown": { redirect: "/usage/markdown" },
+    "/guide/writing/mdc": { redirect: "/usage/markdown" },
+    "/guide/writing/json": { redirect: "/usage/files" },
+    "/guide/writing/yaml": { redirect: "/writing/files" },
+    "/guide/writing/csv": { redirect: "/writing/files" },
+    "/guide/writing/document-driven": {
+      redirect: "/document-driven/introduction",
+    },
+    "/guide/writing/vue-components": {
+      redirect: "/usage/markdown#vue-components",
+    },
+    "/guide/displaying/rendering": { redirect: "/usage/render" },
+    "/guide/displaying/querying": { redirect: "/composables/query-content" },
+    "/guide/displaying/navigation": { redirect: "/usage/navigation" },
+    "/guide/displaying/typescript": { redirect: "/usage/typescript" },
+    "/guide/recipes/sitemap": { redirect: "/recipes/sitemap" },
+    "/guide/deploy/node-server": { redirect: "/get-started/installation" },
+    "/guide/deploy/static-hosting": { redirect: "/get-started/installation" },
+    "/guide/migration/from-v1": { redirect: "/get-started/from-v1" },
+    "/content-v1": { redirect: "/get-started/from-v1" },
+    "/guide/migration/edge-channel": { redirect: "/get-started/edge-channel" },
+    "/api/components/content-doc": { redirect: "/components/content-doc" },
+    "/api/components/content-list": { redirect: "/components/content-list" },
+    "/api/components/content-renderer": {
+      redirect: "/components/content-renderer",
+    },
+    "/api/components/content-navigation": {
+      redirect: "/components/content-navigation",
+    },
+    "/api/components/content-query": { redirect: "/components/content-query" },
+    "/api/components/markdown": { redirect: "/components/content-slot" },
+    "/api/components/content-slot": { redirect: "/components/content-slot" },
+    "/api/components/prose": { redirect: "/components/prose" },
+    "/api/composables/query-content": {
+      redirect: "/composables/query-content",
+    },
+    "/api/composables/fetch-content-navigation": {
+      redirect: "/composables/fetch-content-navigation",
+    },
+    "/api/composables/unwrap": { redirect: "/composables/use-unwrap" },
+    "/api/composables/use-document-driven": {
+      redirect: "/document-driven/use-content",
+    },
+    "/api/composables/use-content-helpers": {
+      redirect: "/composables/use-content-helpers",
+    },
+    "/api/composables/use-content-head": {
+      redirect: "/composables/use-content-head",
+    },
+    "/api/configuration": { redirect: "/get-started/configuration" },
+    "/api/advanced": { redirect: "/recipes/hooks" },
+    "/changelog": { redirect: "https://github.com/nuxt/content/releases" },
+    "/examples/**": { redirect: "/playground" },
+    "/blog/announcing-v2": { redirect: "/" },
+    "/v1": { redirect: "/v1/getting-started/introduction" },
+    // Shortcuts
+    "/document-driven": { redirect: "/document-driven/introduction" },
+  },
+  nitro: {
+    prerender: {
+      routes: ["/api/search.json"],
+    },
+  },
   modules: [
-    "@element-plus/nuxt",
-    "@nuxtjs/tailwindcss",
-    "@vueuse/nuxt",
     "@nuxt/content",
+    "@nuxt/ui",
+    "@nuxthq/studio",
+    "@vueuse/nuxt",
+    "@nuxtjs/fontaine",
+    "@nuxtjs/google-fonts",
+    "nuxt-og-image",
+    "@nuxtjs/plausible",
+    '@element-plus/nuxt'
   ],
-  css: [
-    "element-plus/dist/index.css",
-    "element-plus/theme-chalk/dark/css-vars.css",
-    "~/assets/css/index.scss",
-    "github-markdown-css/github-markdown.css",
-  ],
+  css: ["~/assets/css/index.scss"],
+
+  colorMode: {
+    preference: "light",
+  },
+  ui: {
+    gray: "#f8fafc",
+    icons: ["heroicons", "simple-icons", "ph"],
+    primary: "#f8fafc",
+  },
+
+  fontMetrics: {
+    fonts: ["DM Sans"],
+  },
+
+  googleFonts: {
+    display: "swap",
+    download: true,
+    families: {
+      "DM+Sans": [400, 500, 600, 700],
+    },
+  },
+
+  hooks: {
+    // Related to https://github.com/nuxt/nuxt/pull/22558
+    // Adding all global components to the main entry
+    // To avoid lagging during page navigation on client-side
+    "components:extend": function (components) {
+      for (const comp of components) {
+        if (comp.global) {
+          comp.global = "sync";
+        }
+      }
+    },
+  },
 });
