fef4024d2cc34eb7ecdbade533d66bb2ad4e9ea4
fancinate <1585546519@qq.com>
2023-11-04 12:01:32 +0800

    :zap: 添加驱动管理模块

---
+++ spark-yun-frontend/src/views/driver-management/driver.config.ts
@@ -0,0 +1,86 @@
+export interface BreadCrumb {
+    name: string;
+    code: string;
+    hidden?: boolean;
+}
+
+export interface colConfig {
+    prop?: string;
+    title: string;
+    align?: string;
+    showOverflowTooltip?: boolean;
+    customSlot?: string;
+    width?: number;
+    minWidth?: number;
+}
+
+export interface Pagination {
+    currentPage: number;
+    pageSize: number;
+    total: number;
+}
+
+export interface TableConfig {
+    tableData: Array<any>;
+    colConfigs: Array<colConfig>;
+    seqType: string;
+    pagination?: Pagination; // 分页数据
+    loading?: boolean; // 表格loading
+}
+
+export interface SerchParams {
+    page: number;
+    pageSize: number;
+    searchKeyWord: string;
+}
+
+export const BreadCrumbList: Array<BreadCrumb> = [
+    {
+        name: '作业流',
+        code: 'workflow'
+    }
+]
+
+export const colConfigs: colConfig[] = [
+    {
+        prop: 'name',
+        title: '驱动名称',
+        minWidth: 100,
+        showOverflowTooltip: true
+    },
+    {
+      prop: 'dbType',
+      title: '类型',
+      minWidth: 100,
+      showOverflowTooltip: true
+    },
+    {
+      prop: 'fileName',
+      title: '驱动文件',
+      minWidth: 100,
+      showOverflowTooltip: true
+    },
+    {
+        prop: 'remark',
+        title: '备注',
+        minWidth: 100
+    },
+    {
+        title: '操作',
+        align: 'center',
+        customSlot: 'options',
+        width: 60
+    }
+]
+
+export const TableConfig: TableConfig = {
+    tableData: [],
+    colConfigs: colConfigs,
+    pagination: {
+      currentPage: 1,
+      pageSize: 10,
+      total: 0
+    },
+    seqType: 'seq',
+    loading: false
+}
