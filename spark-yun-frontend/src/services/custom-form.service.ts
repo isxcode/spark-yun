7553136dc932baa85f3c28be78577726130d59a9
fancinate <1585546519@qq.com>
2024-01-06 12:14:02 +0800

    :zap: 自定义表单查询对接

--- spark-yun-frontend/src/services/custom-form.service.ts
+++ spark-yun-frontend/src/services/custom-form.service.ts
@@ -1,10 +1,19 @@
 import { http } from '@/utils/http'
 
-// demo
-export function GetDataSourceTables(params: any): Promise<any> {
+// 创建表单
+export function CreateCustomFormData(params: any): Promise<any> {
     return http.request({
         method: 'post',
-        url: '',
+        url: '/vip/form/addForm',
+        params: params
+    })
+}
+
+// 查询表单
+export function QueryCustomFormList(params: any): Promise<any> {
+    return http.request({
+        method: 'post',
+        url: '/vip/form/pageForm',
         params: params
     })
 }
