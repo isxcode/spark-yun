ac56782dfe92ec212bd9fcdcb216aa7fc63b6225
ispong <ispong@outlook.com>
2024-01-06 22:07:01 +0800

    :sparkles: 表单创建接口

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/form/pojos/req/AddFormReq.java
@@ -0,0 +1,25 @@
+package com.isxcode.star.api.form.pojos.req;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class AddFormReq {
+
+  @Schema(title = "表单名称", example = "测试表单")
+  @NotEmpty(message = "name不能为空")
+  private String name;
+
+  @Schema(title = "数据源id", example = "1234567")
+  @NotEmpty(message = "datasourceId不能为空")
+  private String datasourceId;
+
+  @Schema(title = "表单基于的主表", example = "table1")
+  @NotEmpty(message = "mainTable不能为空")
+  private String mainTable;
+
+  @Schema(title = "备注", example = "备注123")
+  private String remark;
+}
--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/form/pojos/req/FomSaveComponentsReq.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/form/pojos/req/FomSaveComponentsReq.java
@@ -10,19 +10,19 @@ import java.util.List;
 @Data
 public class FomSaveComponentsReq {
 
-	@Schema(title = "表单唯一id", example = "sy_fd34e4a53db640f5943a4352c4d549b9")
-	@NotEmpty(message = "formId不能为空")
-	private String formId;
+  @Schema(title = "表单唯一id", example = "sy_fd34e4a53db640f5943a4352c4d549b9")
+  @NotEmpty(message = "formId不能为空")
+  private String formId;
 
-	private String insertSql;
+  private String insertSql;
 
-	private String deleteSql;
+  private String deleteSql;
 
-	private String updateSql;
+  private String updateSql;
 
-	private String selectSql;
+  private String selectSql;
 
-	@NotEmpty(message = "组件不能为空")
-	@Schema(title = "组件列表")
-	private List<FomComponentDto> components;
+  @NotEmpty(message = "组件不能为空")
+  @Schema(title = "组件列表")
+  private List<FomComponentDto> components;
 }
