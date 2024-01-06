a35a64d4915777e1917b28debf953cb5b865de03
ispong <ispong@outlook.com>
2024-01-06 14:27:38 +0800

    :bug: 修复表单缺少remark字段

--- spark-yun-backend/spark-yun-main/src/main/resources/db/migration/h2/V0.9.0__388.sql
+++ spark-yun-backend/spark-yun-main/src/main/resources/db/migration/h2/V0.9.0__388.sql
@@ -1,7 +1,3 @@
 -- 新增字段 createMode 创建模式
 alter table SY_FORM
   add create_mode varchar(100) null comment '表单创建模式' after status;
-
--- 新增字段 createMode 创建模式
-alter table SY_FORM
-  add remark varchar(500) null comment '表单备注' after create_mode;
