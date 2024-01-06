44f5a87f022f9e5b144676a5e6ba4a3425a6adf0
fancinate <1585546519@qq.com>
2024-01-06 14:28:34 +0800

    :zap: 表单接口对接02

--- spark-yun-frontend/src/services/custom-form.service.ts
+++ spark-yun-frontend/src/services/custom-form.service.ts
@@ -17,3 +17,48 @@ export function QueryCustomFormList(params: any): Promise<any> {
         params: params
     })
 }
+
+// 查询表单配置
+export function QueryFormConfigById(params: any): Promise<any> {
+    return http.request({
+        method: 'post',
+        url: '/vip/form/getForm',
+        params: params
+    })
+}
+
+// 保存表单配置
+export function SaveFormConfigData(params: any): Promise<any> {
+    return http.request({
+        method: 'post',
+        url: '/vip/form/saveFormComponents',
+        params: params
+    })
+}
+
+// 生成的表单查询列表
+export function QueryFormDataList(params: any): Promise<any> {
+    return http.request({
+        method: 'post',
+        url: '/vip/form/queryData',
+        params: params
+    })
+}
+
+// 生成的表单-添加数据
+export function AddFormData(params: any): Promise<any> {
+    return http.request({
+        method: 'post',
+        url: '/vip/form/addData',
+        params: params
+    })
+}
+
+// 生成的表单-更新数据
+export function UpdateFormData(params: any): Promise<any> {
+    return http.request({
+        method: 'post',
+        url: '/vip/form/updateData',
+        params: params
+    })
+}
