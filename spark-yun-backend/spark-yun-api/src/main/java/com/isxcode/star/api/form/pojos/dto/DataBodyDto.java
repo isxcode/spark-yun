619fed45426802a1e885d912a35556a9a7c5243d
ispong <ispong@outlook.com>
2024-01-07 12:50:23 +0800

    :sparkles: 表单数据查询接口

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/form/pojos/dto/DataBodyDto.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/form/pojos/dto/DataBodyDto.java
@@ -1,8 +1,10 @@
 package com.isxcode.star.api.form.pojos.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class DataBodyDto {
 
 	private String value;
