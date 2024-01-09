2b8da1ba8918267da491212c6285928b072ff52a
fancinate <1585546519@qq.com>
2024-01-09 22:39:54 +0800

    :zap: 自定义表单数据添加对接

--- spark-yun-frontend/src/views/custom-form/custom-form-query/add-modal/index.vue
+++ spark-yun-frontend/src/views/custom-form/custom-form-query/add-modal/index.vue
@@ -1,21 +1,34 @@
 <template>
     <BlockModal :model-config="modelConfig">
-        <z-form-engine
-            ref="formEngineRef"
-            v-model="formData"
-            renderSence="new"
-            :formConfigList="formConfigList"
-        ></z-form-engine>
+        <LoadingPage :visible="loading" :network-error="networkError" @loading-refresh="initData(false)">
+            <z-form-engine
+                ref="formEngineRef"
+                v-model="formData"
+                renderSence="new"
+                :formConfigList="formConfigList"
+            ></z-form-engine>
+        </LoadingPage>
     </BlockModal>
 </template>
   
 <script lang="ts" setup>
 import { reactive, defineExpose, ref, nextTick } from 'vue'
+import LoadingPage from '@/components/loading/index.vue'
 import ZFormEngine from '@/lib/packages/z-form-engine/index.vue'
 import { ElMessage } from 'element-plus'
+import { QueryFormConfigById } from '@/services/custom-form.service';
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
 
 const callback = ref<any>()
 const formEngineRef = ref()
+const formData = ref({})
+const formConfigList = ref([])
+
+const loading = ref(false)
+const networkError = ref(false)
+
 const modelConfig = reactive({
     title: '添加',
     visible: false,
@@ -35,198 +48,11 @@ const modelConfig = reactive({
     zIndex: 1100,
     closeOnClickModal: false
 })
-const formData = ref({})
-const formConfigList = reactive([
-  {
-    "uuid": "cdd90901-1d54-ae12-0595-fc8a415b4c64",
-    "type": "simple",
-    "formValueCode": "1",
-    "codeType": "custom",
-    "label": "文本输入",
-    "placeholder": "请输入",
-    "disabled": false,
-    "required": false,
-    "isColumn": true,
-    "width": 2,
-    "componentType": "FormInputText",
-    "valid": true,
-    "icon": "Document",
-    "name": "文本输入"
-  },
-  {
-    "uuid": "9fed36f1-b031-1ffa-2ad9-2446c375c8fe",
-    "type": "simple",
-    "formValueCode": "2",
-    "codeType": "custom",
-    "label": "下拉选择",
-    "placeholder": "请选择",
-    "disabled": false,
-    "multiple": false,
-    "isColumn": true,
-    "width": 2,
-    "componentType": "FormInputSelect",
-    "options": [
-      {
-        "label": "选项1",
-        "value": "1"
-      }
-    ],
-    "valid": true,
-    "icon": "Document",
-    "name": "下拉选择"
-  },
-  {
-        uuid: '2c82156f-2cc2-abe1-f7fd-10bc3671774e',
-        type: 'simple',
-        formValueCode: '333',
-        codeType: 'custom',
-        label: '金额输入',
-        placeholder: '请输入',
-        disabled: false,
-        required: false,
-        isColumn: true,
-        precision: 2,
-        width: 2,
-        componentType: 'FormInputMoney',
-        valid: false,
-        icon: 'Document',
-        name: '金额输入'
-    },
-    {
-        uuid: '323515c0-ea7c-e5e1-ae28-9f37dd1d281f',
-        type: 'simple',
-        formValueCode: '1111',
-        codeType: 'custom',
-        label: '手机号输入',
-        placeholder: '请输入',
-        disabled: false,
-        required: true,
-        defaultValue: '13308749289',
-        isColumn: true,
-        width: 2,
-        componentType: 'FormInputPhone',
-        valid: false,
-        icon: 'Document',
-        name: '手机号输入'
-    },
-    {
-        uuid: '942365e6-938c-5694-ea81-8679d4ced15b',
-        type: 'simple',
-        formValueCode: '333dd',
-        codeType: 'custom',
-        label: '邮箱输入',
-        placeholder: '请输入',
-        disabled: false,
-        required: true,
-        isColumn: true,
-        defaultValue: '123@qq.com',
-        width: 2,
-        componentType: 'FormInputEmail',
-        valid: false,
-        icon: 'Document',
-        name: '邮箱输入'
-    },
-  {
-    "uuid": "3bc1a3d8-4844-5524-9730-b5674cee01c4",
-    "type": "simple",
-    "formValueCode": "3",
-    "codeType": "custom",
-    "label": "数字输入",
-    "placeholder": "请输入",
-    "disabled": false,
-    "required": false,
-    "isColumn": true,
-    "precision": null,
-    "width": 2,
-    "componentType": "FormInputNumber",
-    "valid": true,
-    "icon": "Document",
-    "name": "数字输入"
-  },
-  {
-    "uuid": "0b71d994-56cd-a063-70d2-7ff7aa505fcd",
-    "type": "simple",
-    "formValueCode": "4",
-    "codeType": "custom",
-    "label": "日期选择",
-    "placeholder": "请选择",
-    "disabled": false,
-    "required": false,
-    "isColumn": true,
-    "width": 1,
-    "dateType": "date",
-    "componentType": "FormInputDate",
-    "valid": true,
-    "icon": "Document",
-    "name": "日期选择"
-  },
-  {
-    "uuid": "b52d09d6-43e9-86d6-db3f-315038f95e8e",
-    "type": "simple",
-    "formValueCode": "5",
-    "codeType": "custom",
-    "label": "开关组件",
-    "disabled": false,
-    "required": false,
-    "width": 2,
-    "isColumn": true,
-    "componentType": "FormInputSwitch",
-    "switchInfo": {
-      "open": "是",
-      "close": "否"
-    },
-    "valid": true,
-    "icon": "Open",
-    "name": "开关组件"
-  },
-  {
-    "uuid": "635ccf08-8001-2585-b166-8fdad5a75d76",
-    "type": "simple",
-    "formValueCode": "6",
-    "codeType": "custom",
-    "label": "单选框",
-    "placeholder": "请选择",
-    "disabled": false,
-    "width": 4,
-    "isColumn": true,
-    "componentType": "FormInputRadio",
-    "options": [
-      {
-        "label": "选项1",
-        "value": "1"
-      }
-    ],
-    "valid": true,
-    "icon": "Document",
-    "name": "单选框"
-  },
-  {
-    "uuid": "5dac13bb-5ae7-2980-0a70-74fcf250c267",
-    "type": "simple",
-    "formValueCode": "7",
-    "codeType": "custom",
-    "label": "多选框",
-    "placeholder": "请选择",
-    "disabled": false,
-    "width": 4,
-    "multiple": true,
-    "componentType": "FormInputCheckbox",
-    "isColumn": true,
-    "options": [
-      {
-        "label": "选项1",
-        "value": "1"
-      }
-    ],
-    "valid": true,
-    "icon": "Document",
-    "name": "多选框",
-    "defaultValue": []
-  }
-] )
+
 function showModal(cb: () => void, data?: any): void {
     callback.value = cb
     formData.value = {}
+    getFormConfigById()
     if (data) {
       modelConfig.title = '编辑'
     } else {
@@ -235,11 +61,34 @@ function showModal(cb: () => void, data?: any): void {
     modelConfig.visible = true
 }
 
+function getFormConfigById(tableLoading?: boolean) {
+    loading.value = tableLoading ? false : true
+    networkError.value = networkError.value || false
+    QueryFormConfigById({
+        formId: route.query.id
+    }).then((res: any) => {
+        formConfigList.value = res.data?.components
+    }).catch(() => {
+        loading.value = false
+        networkError.value = true
+    })
+}
+
 function okEvent() {
   formEngineRef.value.validateForm((valid: boolean) => {
     if (valid) {
-      console.log('表单保存', JSON.parse(JSON.stringify(formData.value)))
-      modelConfig.visible = false
+        modelConfig.okConfig.loading = true
+        callback.value(formData.value).then((res: any) => {
+            modelConfig.okConfig.loading = false
+            if (res === undefined) {
+                modelConfig.visible = false
+            } else {
+                modelConfig.visible = true
+            }
+        })
+        .catch(() => {
+            modelConfig.okConfig.loading = false
+        })
     } else {
       ElMessage.warning('请将表单输入完整')
     }
