bd09b4fb24b579889871ad80324f7a0eeb6407f2
ispong <ispong@outlook.com>
2024-01-15 11:47:18 +0800

    :sparkles: 修复sql无法执行

--- spark-yun-backend/spark-yun-main/src/main/resources/db/migration/h2/V0.9.4__388.sql
+++ spark-yun-backend/spark-yun-main/src/main/resources/db/migration/h2/V0.9.4__388.sql
@@ -1,5 +1,5 @@
 alter table SY_FORM
-    add create_mode varchar(50) not null comment '该表单主表的创建模式' after form_version;
+    add create_mode varchar(50) comment '该表单主表的创建模式' after form_version;
 
 alter table SY_API
     alter page_type boolean null comment '分页状态';
