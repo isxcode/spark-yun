44c449596eb937d8f8ad157748e390e2a186ae73
ispong <ispong@outlook.com>
2024-01-25 18:43:07 +0800

    修复数据查询兼容静态组件

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/form/constants/FormComponentType.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/form/constants/FormComponentType.java
@@ -2,22 +2,63 @@ package com.isxcode.star.api.form.constants;
 
 public interface FormComponentType {
 
+	/**
+	 * 下拉.
+	 */
 	String FORM_INPUT_SELECT = "FormInputSelect";
 
+	/**
+	 * 数字.
+	 */
 	String FORM_INPUT_NUMBER = "FormInputNumber";
 
+	/**
+	 * 金额.
+	 */
 	String FORM_INPUT_MONEY = "FormInputMoney";
 
+	/**
+	 * 日期.
+	 */
 	String FORM_INPUT_DATE = "FormInputDate";
 
+	/**
+	 * 开关.
+	 */
 	String FORM_INPUT_SWITCH = "FormInputSwitch";
 
+	/**
+	 * 单选.
+	 */
 	String FORM_INPUT_RADIO = "FormInputRadio";
 
+	/**
+	 * 多选.
+	 */
 	String FORM_INPUT_CHECKBOX = "FormInputCheckbox";
 
+	/**
+	 * 邮箱.
+	 */
 	String FORM_INPUT_EMAIL = "FormInputEmail";
 
+	/**
+	 * 手机号.
+	 */
 	String FORM_INPUT_PHONE = "FormInputPhone";
 
+	/**
+	 * 静态文本.
+	 */
+	String FORM_STATIC_PLACEHOLDER = "FormStaticPlaceholder";
+
+	/**
+	 * 占位符.
+	 */
+	String FORM_STATIC_EMPTY = "FormStaticEmpty";
+
+	/**
+	 * 文本.
+	 */
+	String FORM_INPUT_TEXT = "FormInputText";
 }
