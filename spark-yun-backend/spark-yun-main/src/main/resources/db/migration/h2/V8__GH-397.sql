9a49468a80620101fca34bbcf71b035630ebe398
ispong <ispong@outlook.com>
2024-02-03 21:38:58 +0800

    修复建表sql

--- spark-yun-backend/spark-yun-main/src/main/resources/db/migration/h2/V8__GH-397.sql
+++ spark-yun-backend/spark-yun-main/src/main/resources/db/migration/h2/V8__GH-397.sql
@@ -9,7 +9,7 @@ create table SY_FORM_LINK
   form_id                 varchar(200)  not null comment '表单id',
   form_version            varchar(200)  not null comment '表单版本',
   form_token              varchar(500)  not null comment '分享表单的匿名token',
-  invalid_data_time       datetime      not null comment '到期时间',
+  invalid_date_time       datetime      not null comment '到期时间',
   create_by               varchar(200)  not null comment '创建人',
   create_date_time        datetime      not null comment '创建时间',
   last_modified_by        varchar(200)  not null comment '更新人',
