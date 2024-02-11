27f300468df411dc1f52beb48b709b7e12873256
ispong <ispong@outlook.com>
2024-02-11 22:28:59 +0800

    GH-472 添加工具类，日志等级，缓存查询和许可证逻辑

--- spark-yun-backend/spark-yun-common/src/main/java/com/isxcode/star/common/version/ToolController.java
+++ spark-yun-backend/spark-yun-common/src/main/java/com/isxcode/star/common/version/ToolController.java
@@ -3,17 +3,19 @@ package com.isxcode.star.common.version;
 import com.isxcode.star.backend.api.base.exceptions.IsxAppException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.cache.CacheManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.util.Locale;
+import java.util.Objects;
 
 @Tag(name = "工具模块")
 @RestController
@@ -42,4 +44,29 @@ public class ToolController {
 			throw new IsxAppException("获取版本号异常", e.getMessage());
 		}
 	}
+
+	@Operation(summary = "切换系统日志等级")
+	@GetMapping("/setLogLevel")
+	public String setLogLevel(@RequestParam String level) {
+
+		LoggingSystem system = LoggingSystem.get(LoggingSystem.class.getClassLoader());
+		LogLevel logLevel = LogLevel.valueOf(level.trim().toUpperCase(Locale.ENGLISH));
+		system.setLogLevel("com.isxcode.star", logLevel);
+		return "当前日志等级：" + logLevel.name();
+	}
+
+	@Operation(summary = "获取缓存列表")
+	@GetMapping("/getCacheList")
+	public String getCacheList() {
+
+		return cacheManager.getCacheNames().toString();
+	}
+
+	@Operation(summary = "获取制定缓存信息")
+	@GetMapping("/getCache")
+	public String getCache(@RequestParam String name) {
+
+		return Objects.requireNonNull(cacheManager.getCache(name)).getNativeCache().toString();
+	}
+
 }
