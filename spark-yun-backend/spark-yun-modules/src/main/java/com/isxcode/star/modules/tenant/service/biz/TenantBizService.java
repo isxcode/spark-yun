a18db72c87eb0664349537599b42256f4bd908ae
ispong <ispong@outlook.com>
2024-02-22 14:35:46 +0800

    租户中的用户限制和作业流限制生效

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/tenant/service/biz/TenantBizService.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/tenant/service/biz/TenantBizService.java
@@ -130,8 +130,14 @@ public class TenantBizService {
 		Page<TenantEntity> tenantEntityPage = tenantRepository.searchAll(tetQueryTenantReq.getSearchKeyWord(),
 				PageRequest.of(tetQueryTenantReq.getPage(), tetQueryTenantReq.getPageSize()));
 
-		return tenantEntityPage.map(tenantMapper::tenantEntityToTetQueryTenantRes);
-	}
+    Page<PageTenantRes> result = tenantEntityPage.map(tenantMapper::tenantEntityToTetQueryTenantRes);
+    JPA_TENANT_MODE.set(false);
+    result.getContent().forEach(e -> {
+      e.setUsedWorkflowNum(String.valueOf(workflowRepository.countByTenantId(e.getId())));
+      e.setUsedMemberNum(String.valueOf(tenantUserRepository.countByTenantId(e.getId())));
+    });
+    return result;
+  }
 
 	public void updateTenantForSystemAdmin(UpdateTenantForSystemAdminReq tetUpdateTenantBySystemAdminReq) {
 
--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/workflow/service/WorkflowBizService.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/workflow/service/WorkflowBizService.java
@@ -25,6 +25,9 @@ import com.isxcode.star.common.locker.Locker;
 import com.isxcode.star.modules.cluster.entity.ClusterEntity;
 import com.isxcode.star.modules.cluster.repository.ClusterRepository;
 import com.isxcode.star.modules.cluster.service.ClusterService;
+import com.isxcode.star.modules.tenant.entity.TenantEntity;
+import com.isxcode.star.modules.tenant.repository.TenantRepository;
+import com.isxcode.star.modules.tenant.service.TenantService;
 import com.isxcode.star.modules.work.entity.WorkConfigEntity;
 import com.isxcode.star.modules.work.entity.WorkEntity;
 import com.isxcode.star.modules.work.entity.WorkExportInfo;
@@ -105,6 +108,8 @@ public class WorkflowBizService {
 
 	private final ClusterService clusterService;
 
+  private final TenantService tenantService;
+
 	public WorkflowEntity getWorkflowEntity(String workflowId) {
 
 		Optional<WorkflowEntity> workflowEntityOptional = workflowRepository.findById(workflowId);
@@ -116,7 +121,14 @@ public class WorkflowBizService {
 
 	public void addWorkflow(AddWorkflowReq wofAddWorkflowReq) {
 
-		// 工作流唯一性
+    // 判断租户下的作业流上限
+    TenantEntity tenant = tenantService.getTenant(TENANT_ID.get());
+    long workflowCount = workflowRepository.count();
+    if (workflowCount + 1 > tenant.getMaxWorkflowNum()) {
+      throw new IsxAppException("超出租户的最大作业流限制");
+    }
+
+    // 工作流唯一性
 		Optional<WorkflowEntity> workflowByName = workflowRepository.findByName(wofAddWorkflowReq.getName());
 		if (workflowByName.isPresent()) {
 			throw new IsxAppException("作业流名称已重复，请重新输入");
