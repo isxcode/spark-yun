9d61c8182539cff6ab79563745176b81f1b6a4b8
ispong <ispong@outlook.com>
2024-02-26 18:56:40 +0800

    完成首页数据监听功能

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/monitor/dto/MonitorLineDto.java
@@ -0,0 +1,48 @@
+package com.isxcode.star.api.monitor.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.isxcode.star.backend.api.base.serializer.LocalDateMinuteSerializer;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class MonitorLineDto {
+
+	private Long activeNodeSize;
+
+	private String tenantId;
+
+	@JsonSerialize(using = LocalDateMinuteSerializer.class)
+	private LocalDateTime dateTime;
+
+	private Double cpuPercent;
+
+	private Long diskIoReadSpeed;
+
+	private Long networkIoReadSpeed;
+
+	private Long diskIoWriteSpeed;
+
+	private Long networkIoWriteSpeed;
+
+	private Long usedMemorySize;
+
+	private Long usedStorageSize;
+
+	public MonitorLineDto(Long activeNodeSize, String tenantId, LocalDateTime dateTime, Double cpuPercent,
+			Long diskIoReadSpeed, Long networkIoReadSpeed, Long diskIoWriteSpeed, Long networkIoWriteSpeed,
+			Long usedMemorySize, Long usedStorageSize) {
+
+		this.activeNodeSize = activeNodeSize;
+		this.tenantId = tenantId;
+		this.dateTime = dateTime;
+		this.cpuPercent = cpuPercent;
+		this.diskIoReadSpeed = diskIoReadSpeed;
+		this.networkIoReadSpeed = networkIoReadSpeed;
+		this.diskIoWriteSpeed = diskIoWriteSpeed;
+		this.networkIoWriteSpeed = networkIoWriteSpeed;
+		this.usedMemorySize = usedMemorySize;
+		this.usedStorageSize = usedStorageSize;
+	}
+}
