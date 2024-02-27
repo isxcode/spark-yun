86addd9e8408cce3becec5086c0ff9c769a14261
ispong <ispong@outlook.com>
2024-02-27 16:48:59 +0800

    首页租户基础信息查询

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/monitor/dto/SystemMonitorDto.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/monitor/dto/SystemMonitorDto.java
@@ -1,14 +1,16 @@
 package com.isxcode.star.api.monitor.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class SystemMonitorDto {
 
 	@Schema(title = "卡片总数")
-	private Integer total;
+	private Long total;
 
 	@Schema(title = "激活数")
-	private Integer activeNum;
+	private Long activeNum;
 }
