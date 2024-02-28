b570e1a36eb0320983d930d8c6c1518e7cb9f717
ispong <ispong@outlook.com>
2024-02-28 10:33:08 +0800

    优化信息监控

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/monitor/pojos/dto/MonitorLineDto.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/monitor/pojos/dto/MonitorLineDto.java
@@ -2,6 +2,7 @@ package com.isxcode.star.api.monitor.pojos.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.isxcode.star.backend.api.base.serializer.LocalDateMinuteSerializer;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,25 +10,35 @@ import java.time.LocalDateTime;
 @Data
 public class MonitorLineDto {
 
+	@Schema(title = "激活节点个数")
 	private Long activeNodeSize;
 
+	@Schema(title = "租户id")
 	private String tenantId;
 
+	@Schema(title = "当前时间")
 	@JsonSerialize(using = LocalDateMinuteSerializer.class)
 	private LocalDateTime dateTime;
 
+	@Schema(title = "cpu占用率")
 	private Double cpuPercent;
 
+	@Schema(title = "磁盘io读速度")
 	private Long diskIoReadSpeed;
 
+	@Schema(title = "网络io读速度")
 	private Long networkIoReadSpeed;
 
+	@Schema(title = "磁盘io写速度")
 	private Long diskIoWriteSpeed;
 
+	@Schema(title = "网络io写速度")
 	private Long networkIoWriteSpeed;
 
+	@Schema(title = "内存使用")
 	private Long usedMemorySize;
 
+	@Schema(title = "存储使用")
 	private Long usedStorageSize;
 
 	public MonitorLineDto(Long activeNodeSize, String tenantId, LocalDateTime dateTime, Double cpuPercent,
