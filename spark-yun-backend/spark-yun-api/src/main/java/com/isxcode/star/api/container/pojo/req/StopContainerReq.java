ce8067c6ca7d209c17e1b3596c3ed9f01151b0e3
ispong <ispong@outlook.com>
2024-03-10 12:19:32 +0800

    优化spark容器作业

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/container/pojo/req/StopContainerReq.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/container/pojo/req/StopContainerReq.java
@@ -1,13 +1,17 @@
 package com.isxcode.star.api.container.pojo.req;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StopContainerReq {
 
 	@Schema(title = "containerId", example = "sy_213")
