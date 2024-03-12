b10620e719885beb6b999579ac4e316e9f6fdd4b
aoyia <>
2024-03-12 18:54:19 +0800

    fix: 去除img

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -3,14 +3,10 @@ import path from "path";
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ["@nuxt/content", "@pinia/nuxt", "@vueuse/nuxt", "@element-plus/nuxt", "@nuxtjs/tailwindcss", "nuxt-lodash"],
-  head: {
-    script: [{ src: "~/static/js/flexible.js", type: "text/javascript" }],
-  },
   pinia: {
     autoImports: ["defineStore"],
   },
-  // 引入plugins
-  plugins: [{ src: "~/plugins/svgicon.client.ts" }, { src: "~/plugins/lib-flexible.ts" }],
+  plugins: [{ src: "~/plugins/svgicon.client.ts" }],
   css: ["element-plus/dist/index.css", "element-plus/theme-chalk/dark/css-vars.css", "~/assets/css/index.scss"],
   vite: {
     plugins: [
