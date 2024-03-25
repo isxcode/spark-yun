c131485114689040adaca6e479d3cd1110c5a955
ispong <ispong@outlook.com>
2024-03-25 15:16:14 +0800

    修复实时同步日志异常问题

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/real/pojos/res/GetRealRunningLogRes.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/real/pojos/res/GetRealRunningLogRes.java
@@ -1,15 +1,19 @@
 package com.isxcode.star.api.real.pojos.res;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class GetRealRunningLogRes {
 
 	private String id;
 
 	private String runningLog;
+
+  private String status;
 }
