5cb2877320feec8f48d5331ac92d266ecf7c795c
Forzh <hao.zhang5@definesys.com>
2024-04-03 17:40:50 +0800

    local模式执行作业报错问题修复

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/SparkSqlExecutor.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/SparkSqlExecutor.java
@@ -217,7 +217,11 @@ public class SparkSqlExecutor extends WorkExecutor {
 		executeReq.setSparkSubmit(sparkSubmit);
 		executeReq.setPluginReq(pluginReq);
 		executeReq.setAgentHomePath(engineNode.getAgentHomePath() + File.separator + PathConstants.AGENT_PATH_NAME);
-		executeReq.setSparkHomePath(engineNode.getSparkHomePath());
+    if(engineNode.getInstallSparkLocal()) {
+      executeReq.setSparkHomePath(engineNode.getAgentHomePath() + File.separator + PathConstants.AGENT_PATH_NAME + File.separator + PathConstants.SPARK_MIN_HOME);
+    } else {
+      executeReq.setSparkHomePath(engineNode.getSparkHomePath());
+    }
 		executeReq.setAgentType(calculateEngineEntityOptional.get().getClusterType());
 
 		// 构建作业完成，并打印作业配置信息
