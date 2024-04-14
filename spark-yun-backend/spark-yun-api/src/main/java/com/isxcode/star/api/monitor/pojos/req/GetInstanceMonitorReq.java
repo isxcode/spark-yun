2261eb2d2e499771a286244497e131bb6b3f6609
ispong <ispong@outlook.com>
2024-04-15 00:45:44 +0800

    修复前端的日期传递异常

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/monitor/pojos/req/GetInstanceMonitorReq.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/monitor/pojos/req/GetInstanceMonitorReq.java
@@ -1,5 +1,7 @@
 package com.isxcode.star.api.monitor.pojos.req;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.isxcode.star.backend.api.base.serializer.LocalDateSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -11,5 +13,6 @@ public class GetInstanceMonitorReq {
 
 	@Schema(title = "日期时间，例如2020-12-12", example = "2020-12-12")
 	@NotNull(message = "localDate不能为空")
+  @JsonSerialize(using = LocalDateSerializer.class)
 	private Date localDate;
 }
