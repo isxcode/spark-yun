9e458a9ab72065c7146dfd98a27ac9eaffe6482b
ispong <ispong@outlook.com>
2024-04-15 00:00:08 +0800

    GH-676 首页监控,系统信息异常
    Co-authored-by: Dreamfish <shunchang.jia@definesys.com>

--- spark-yun-frontend/components.d.ts
+++ spark-yun-frontend/components.d.ts
@@ -29,6 +29,7 @@ declare module 'vue' {
     ElDropdown: typeof import('element-plus/es')['ElDropdown']
     ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
+    ElEmpty: typeof import('element-plus/es')['ElEmpty']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
