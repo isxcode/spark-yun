854ba29c188851f17833eb4012985002a472756d
ispong <ispong@outlook.com>
2024-04-14 23:38:20 +0800

    GH-466 分享表单，更新数据，多选框，传值异常
    * 发布0.9.0版本
    
    * GH-499 自定义接口测试，后端写死了8080端口号
    
    * 修复接口测试端口号异常
    
    * 格式化代码
    
    * 默认关闭jpa的sql执行打印
    
    * bug：分享表单，修改金额组件不回显的问题
    
    * bug: 登录：添加回车登录
    
    * bug(394): 登录页面，总是弹出用户登录过期字样
    
    * 后端多选添加label和value值
    
    * 格式化代码
    
    * 删除DS_Store
    
    * 登出提示修改与其它分支重复
    
    * 表单适配多选开关组件
    
    * 格式化代码
    
    * 删除DS_store
    
    * 合并main分支
    
    * 删除.DS_Store
    
    ---------
    
    Co-authored-by: fancinate <1585546519@qq.com>

---
+++ spark-yun-backend/spark-yun-main/src/main/resources/db/release/h2/V0.9.0__6-8.sql
@@ -0,0 +1,108 @@
+-- 新增udf定义表
+create table SY_WORK_UDF
+(
+  id                      varchar(200)  not null comment 'udf唯一id'
+    primary key,
+  type                    varchar(20)   not null comment 'udf类型',
+  file_id                 varchar(200)  not null comment 'jar文件id',
+  func_name               varchar(200)  not null comment 'udf调用方法名',
+  class_name              varchar(200)  not null comment 'udf方法class名称',
+  result_type             varchar(200)  not null comment '返回值类型',
+  status                  boolean       not null comment '是否启用此udf',
+  create_by               varchar(200)  not null comment '创建人',
+  create_date_time        datetime      not null comment '创建时间',
+  last_modified_by        varchar(200)  not null comment '更新人',
+  last_modified_date_time datetime      not null comment '更新时间',
+  version_number          int           not null comment '版本号',
+  deleted                 int default 0 not null comment '逻辑删除',
+  tenant_id               varchar(200)  not null comment '租户id'
+);
+
+-- 新增字段 createMode 创建模式
+alter table SY_FORM
+  add create_mode varchar(100) null comment '表单创建模式' after status;
+
+-- 表单新增表单备注字段
+alter table SY_FORM
+  add remark varchar(500) null comment '表单备注' after create_mode;
+
+alter table SY_FORM
+    alter insert_sql varchar(2000) null comment '增sql语句';
+
+alter table SY_FORM
+    alter delete_sql varchar(2000) null comment '删sql语句';
+
+alter table SY_FORM
+    alter update_sql varchar(2000) null comment '改sql语句';
+
+alter table SY_FORM
+    alter select_sql varchar(2000) null comment '查sql语句';
+
+alter table SY_FORM
+    drop column create_mode;
+
+alter table SY_FORM
+    add form_web_config longtext null comment '前端所有的配置' after select_sql;
+
+alter table SY_FORM_COMPONENT
+    add uuid varchar(50) not null comment '前端的uuid' after form_id;;
+
+alter table SY_FORM_COMPONENT
+    add component_config text null comment '组件的配置' after value_sql;
+
+alter table SY_FORM_COMPONENT
+    drop column name;
+
+alter table SY_FORM_COMPONENT
+    drop column component_type;
+
+alter table SY_FORM_COMPONENT
+    drop column component_key;
+
+alter table SY_FORM_COMPONENT
+    drop column is_display;
+
+alter table SY_FORM_COMPONENT
+    drop column is_primary_key;
+
+alter table SY_FORM_COMPONENT
+    drop column show_value;
+
+alter table SY_FORM_COMPONENT
+    drop column value_sql;
+
+alter table SY_FORM
+    add form_version varchar(50) null comment '表单版本号' after form_web_config;
+
+alter table SY_API
+    add token_type varchar(200) null comment '认证方式' after status;
+
+alter table SY_API
+    add page_type varchar(50) null comment '分页状态' after token_type;
+
+alter table SY_FORM
+    add create_mode varchar(50) comment '该表单主表的创建模式' after form_version;
+
+alter table SY_API
+    alter page_type boolean null comment '分页状态';
+
+alter table SY_API
+    alter column API_SQL set null;
+
+-- 添加分享表单的链接表
+create table SY_FORM_LINK
+(
+  id                      varchar(200)  not null comment '分享表单链接id'
+    primary key,
+  form_id                 varchar(200)  not null comment '表单id',
+  form_version            varchar(200)  not null comment '表单版本',
+  form_token              varchar(500)  not null comment '分享表单的匿名token',
+  invalid_date_time       datetime      not null comment '到期时间',
+  create_by               varchar(200)  not null comment '创建人',
+  create_date_time        datetime      not null comment '创建时间',
+  last_modified_by        varchar(200)  not null comment '更新人',
+  last_modified_date_time datetime      not null comment '更新时间',
+  version_number          int           not null comment '版本号',
+  deleted                 int default 0 not null comment '逻辑删除',
+  tenant_id               varchar(200)  not null comment '租户id'
+);
