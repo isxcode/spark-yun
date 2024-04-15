6e56b9efba4d03087cbbdfff4e02e97c0c4494b9
ispong <ispong@outlook.com>
2024-04-15 23:09:07 +0800

    GH-714 资源中心上传文件时，文件为空，直接报接口400
    * 指定后端resource异常
    
    * 替换banner
    
    * 修复登录页面弹窗问题
    
    * 添加前端logo
    
    * 移除VIP的spark-query-sql-plugin
    
    * 首页logo样式替换
    
    * 替换产品logo
    
    * 代理器开启日志归档功能
    
    * 修复官网下的横线
    
    * 删除podTemplate.yml文件

--- spark-yun-dist/zhiqingyun-agent/bin/start.sh
+++ spark-yun-dist/zhiqingyun-agent/bin/start.sh
@@ -11,13 +11,19 @@ cd ".." || exit
 if [ -e "zhiqingyun-agent.pid" ]; then
   pid=$(cat "zhiqingyun-agent.pid")
   if ps -p $pid >/dev/null 2>&1; then
-    echo "【至轻云代理】: RUNNING"
+    echo "【至轻云代理】: HAS RUNNING"
     exit 0
   fi
 fi
 
+# 判断zhiliuyun-agent.log是否存在,不存在则新建
+if [ ! -f logs/zhiliuyun-agent.log ]; then
+  mkdir logs
+  touch logs/zhiliuyun-agent.log
+fi
+
 # 运行jar包
-nohup java -jar -Xmx2048m lib/zhiqingyun-agent.jar --spring.config.additional-location=conf/ >>logs/zhiqingyun-agent.log 2>&1 &
+nohup java -jar -Xmx2048m lib/zhiqingyun-agent.jar --spring.config.additional-location=conf/ > /dev/null 2>&1 &
 echo $! >zhiqingyun-agent.pid
 echo "【至轻云代理】: RUNNING"
 
