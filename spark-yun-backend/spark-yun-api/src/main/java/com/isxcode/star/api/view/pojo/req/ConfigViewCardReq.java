f83aedba7023ddcfec284183c00d374c1801a535
ispong <ispong@outlook.com>
2024-05-11 18:16:46 +0800

    新增大屏组件模块功能

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/view/pojo/req/ConfigViewCardReq.java
@@ -0,0 +1,20 @@
+package com.isxcode.star.api.view.pojo.req;
+
+import com.isxcode.star.api.view.pojo.dto.DataSql;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConfigViewCardReq {
+
+  private String id;
+
+  private Object webConfig;
+
+  private DataSql dataSql;
+}
