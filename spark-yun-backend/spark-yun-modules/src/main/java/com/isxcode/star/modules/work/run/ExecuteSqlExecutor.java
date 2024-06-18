d87a14f7381b2d4ccc207cefa016b84284c3a986
ispong <ispong@outlook.com>
2024-06-18 17:54:37 +0800

    格式化代码

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/ExecuteSqlExecutor.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/ExecuteSqlExecutor.java
@@ -49,7 +49,7 @@ public class ExecuteSqlExecutor extends WorkExecutor {
 	@Override
 	public void execute(WorkRunContext workRunContext, WorkInstanceEntity workInstance) {
 
-    // 将线程存到Map
+		// 将线程存到Map
 		WORK_THREAD.put(workInstance.getId(), Thread.currentThread());
 
 		// 获取日志构造器
--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/SparkContainerSqlExecutor.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/SparkContainerSqlExecutor.java
@@ -57,7 +57,7 @@ public class SparkContainerSqlExecutor extends WorkExecutor {
 
 	public void execute(WorkRunContext workRunContext, WorkInstanceEntity workInstance) {
 
-    // 将线程存到Map
+		// 将线程存到Map
 		WORK_THREAD.put(workInstance.getId(), Thread.currentThread());
 
 		// 获取日志构造器
