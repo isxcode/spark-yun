9fa9de02356280d4381526bb3f2e23ce888f813a
ispong <ispong@outlook.com>
2024-06-27 17:53:57 +0800

    将告警的机制改为单个消息体

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/alarm/repository/AlarmRepository.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/alarm/repository/AlarmRepository.java
@@ -1,6 +1,7 @@
 package com.isxcode.star.modules.alarm.repository;
 
 import com.isxcode.star.modules.alarm.entity.AlarmEntity;
+import com.isxcode.star.modules.alarm.entity.MessageEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,9 +9,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AlarmRepository extends JpaRepository<AlarmEntity, String> {
 
 	@Query("SELECT A FROM AlarmEntity A WHERE A.name LIKE %:keyword% OR A.remark LIKE %:keyword% order by A.createDateTime desc ")
 	Page<AlarmEntity> searchAll(@Param("keyword") String searchKeyWord, Pageable pageable);
+
+  Optional<AlarmEntity> findByName(String name);
 }
--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/alarm/service/AlarmService.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/alarm/service/AlarmService.java
@@ -1,13 +1,17 @@
 package com.isxcode.star.modules.alarm.service;
 
 import com.alibaba.fastjson.JSON;
+import com.isxcode.star.api.alarm.constants.AlarmSendStatus;
+import com.isxcode.star.api.alarm.constants.MessageStatus;
 import com.isxcode.star.api.alarm.dto.MessageConfig;
 import com.isxcode.star.backend.api.base.exceptions.IsxAppException;
 import com.isxcode.star.modules.alarm.entity.AlarmEntity;
+import com.isxcode.star.modules.alarm.entity.AlarmInstanceEntity;
 import com.isxcode.star.modules.alarm.entity.MessageEntity;
 import com.isxcode.star.modules.alarm.message.MessageContext;
 import com.isxcode.star.modules.alarm.message.MessageFactory;
 import com.isxcode.star.modules.alarm.message.MessageRunner;
+import com.isxcode.star.modules.alarm.repository.AlarmInstanceRepository;
 import com.isxcode.star.modules.alarm.repository.AlarmRepository;
 import com.isxcode.star.modules.alarm.repository.MessageRepository;
 import com.isxcode.star.modules.user.service.UserService;
@@ -16,6 +20,7 @@ import com.isxcode.star.modules.work.run.WorkRunContext;
 import com.isxcode.star.security.user.UserEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -23,84 +28,100 @@ import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.isxcode.star.common.config.CommonConfig.TENANT_ID;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
 public class AlarmService {
 
-	private final MessageRepository messageRepository;
+  private final MessageRepository messageRepository;
 
-	private final AlarmRepository alarmRepository;
+  private final AlarmRepository alarmRepository;
 
-	private final MessageFactory messageFactory;
+  private final MessageFactory messageFactory;
 
-	private final UserService userService;
+  private final UserService userService;
 
-	public MessageEntity getMessage(String messageId) {
+  private final AlarmInstanceRepository alarmInstanceRepository;
 
-		return messageRepository.findById(messageId).orElseThrow(() -> new IsxAppException("消息体不存在"));
-	}
+  public MessageEntity getMessage(String messageId) {
 
-	public AlarmEntity getAlarm(String alarmId) {
+    return messageRepository.findById(messageId).orElseThrow(() -> new IsxAppException("消息体不存在"));
+  }
 
-		return alarmRepository.findById(alarmId).orElseThrow(() -> new IsxAppException("告警不存在"));
-	}
+  public AlarmEntity getAlarm(String alarmId) {
 
-	/**
-	 * 异步给定时作业发消息.
-	 */
-	@Async
-	public void sendWorkMessage(WorkRunContext workRunContext, WorkInstanceEntity workInstance, String alarmEvent) {
+    return alarmRepository.findById(alarmId).orElseThrow(() -> new IsxAppException("告警不存在"));
+  }
 
-		// 当配置了告警处理
-		if (workRunContext.getAlarmList() != null && !workRunContext.getAlarmList().isEmpty()) {
+  /**
+   * 异步给定时作业发消息.
+   */
+  @Async
+  public void sendWorkMessage(WorkRunContext workRunContext, WorkInstanceEntity workInstance, String alarmEvent) {
 
-			// 遍历多个告警
-			workRunContext.getAlarmList().forEach(alarmId -> {
+    TENANT_ID.set(workInstance.getTenantId());
 
-				// 查询告警信息
-				AlarmEntity alarm = getAlarm(alarmId);
+    // 当配置了告警处理
+    if (workRunContext.getAlarmList() != null && !workRunContext.getAlarmList().isEmpty()) {
 
-				// 满足当前事件当发送
-				if (alarmEvent.equals(alarm.getAlarmEvent())) {
+      // 遍历多个告警
+      workRunContext.getAlarmList().forEach(alarmId -> {
 
-					// 拼接消息内容
-					String content = alarm.getAlarmTemplate();
+        // 查询告警信息
+        AlarmEntity alarm = getAlarm(alarmId);
 
-					// 获取需要发送的人
-					List<String> receiverList = JSON.parseArray(alarm.getReceiverList(), String.class);
+        // 满足当前事件当发送
+        if (alarmEvent.equals(alarm.getAlarmEvent())) {
 
-					// 获取告警中当消息体
-					if (alarm.getMsgList() != null && !alarm.getMsgList().isEmpty()) {
+          // 拼接消息内容
+          String content = alarm.getAlarmTemplate();
 
-						// 遍历消息体，逐个发送
-						List<String> msgIdList = JSON.parseArray(alarm.getMsgList(), String.class);
-						msgIdList.forEach(msgId -> {
+          // 获取需要发送的人
+          List<String> receiverList = JSON.parseArray(alarm.getReceiverList(), String.class);
 
-							// 构建消息发送的context
-							MessageEntity message = getMessage(msgId);
-							MessageConfig messageConfig = JSON.parseObject(message.getMsgConfig(), MessageConfig.class);
-							MessageRunner messageAction = messageFactory.getMessageAction(message.getMsgType());
-							MessageContext messageContext = MessageContext.builder().alarmType(alarm.getAlarmType())
-									.alarmId(alarmId).alarmEvent(alarmEvent).msgType(message.getMsgType())
-									.messageConfig(messageConfig).tenantId(message.getTenantId()).content(content)
-									.build();
+          // 获取告警中当消息体
+          if (!Strings.isEmpty(alarm.getMsgId())) {
 
-							// 查询联系人的信息，发送消息
-							receiverList.forEach(userId -> {
+            // 构建消息发送的context
+            MessageEntity message = getMessage(alarm.getMsgId());
+            MessageConfig messageConfig = JSON.parseObject(message.getMsgConfig(), MessageConfig.class);
 
-								// 发送时间
-								UserEntity user = userService.getUser(userId);
-								messageContext.setEmail(user.getEmail());
-								messageContext.setReceiver(userId);
-								messageContext.setSendDateTime(LocalDateTime.now());
-								messageAction.send(messageContext);
-							});
-						});
-					}
-				}
-			});
-		}
-	}
+            // 构建消息发送执行期
+            MessageRunner messageRunner = messageFactory.getMessageAction(message.getMsgType());
+            MessageContext messageContext = MessageContext.builder().alarmType(alarm.getAlarmType())
+              .alarmId(alarmId).alarmEvent(alarmEvent).msgType(message.getMsgType())
+              .messageConfig(messageConfig).tenantId(message.getTenantId()).content(content).instanceId(workInstance.getId())
+              .build();
+
+            // 查询联系人的信息，发送消息
+            receiverList.forEach(userId -> {
+
+              // 发送时间
+              UserEntity user = userService.getUser(userId);
+              messageContext.setEmail(user.getEmail());
+              messageContext.setPhone(user.getPhone());
+              messageContext.setReceiver(userId);
+              messageContext.setSendDateTime(LocalDateTime.now());
+
+              // 如果消息体不为可用状态,不发消息
+              if (!MessageStatus.ACTIVE.equals(message.getStatus())) {
+                AlarmInstanceEntity alarmInstanceEntity = AlarmInstanceEntity.builder().alarmId(messageContext.getAlarmId())
+                  .alarmType(messageContext.getAlarmType()).alarmEvent(messageContext.getAlarmEvent())
+                  .msgType(messageContext.getMsgType()).content(messageContext.getContent())
+                  .receiver(messageContext.getReceiver()).instanceId(messageContext.getInstanceId()).sendDateTime(messageContext.getSendDateTime()).build();
+                alarmInstanceEntity.setSendStatus(AlarmSendStatus.FAIL);
+                alarmInstanceEntity.setResponse("消息体不为激活状态");
+                alarmInstanceRepository.save(alarmInstanceEntity);
+              }else{
+                messageRunner.send(messageContext);
+              }
+            });
+          }
+        }
+      });
+    }
+  }
 }
