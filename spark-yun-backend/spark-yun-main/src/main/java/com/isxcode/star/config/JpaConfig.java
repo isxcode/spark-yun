41a40434333f64919e80503f55311742eb6fba85
ispong <ispong@outlook.com>
2024-07-02 18:49:42 +0800

    GH-960 [Improve] 将flyway的链接配置整合到jdbc中
    * 优化settings.gradle文件
    
    * 优化配置文件
    
    * 优化配置文件内容
    
    * 格式化代码
    
    * 优化话术
    
    * 优化安装包部署文档
    
    * 修改版本号

---
+++ spark-yun-backend/spark-yun-main/src/main/java/com/isxcode/star/config/JpaConfig.java
@@ -0,0 +1,51 @@
+package com.isxcode.star.config;
+
+import com.alibaba.druid.util.JdbcUtils;
+import com.isxcode.star.backend.api.base.properties.IsxAppProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.vendor.Database;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Configuration
+@AutoConfigureBefore(HibernateJpaAutoConfiguration.class)
+@EnableConfigurationProperties({DataSourceProperties.class, JpaProperties.class, IsxAppProperties.class})
+@ConditionalOnClass(JdbcUtils.class)
+@RequiredArgsConstructor
+public class JpaConfig {
+
+	private final JpaProperties jpaProperties;
+
+	private final DataSourceProperties dataSourceProperties;
+
+	private final IsxAppProperties isxAppProperties;
+
+	@PostConstruct
+	public void changeJpaProperties() {
+
+		if ("simple".equals(isxAppProperties.getConfigMode())) {
+			if ("org.h2.Driver".equals(dataSourceProperties.getDriverClassName())) {
+				jpaProperties.setDatabase(Database.H2);
+			} else {
+				jpaProperties.setDatabase(Database.MYSQL);
+			}
+		}
+	}
+
+	@Bean
+	@Primary
+	public JpaProperties jpaProperties() {
+		return jpaProperties;
+	}
+}
