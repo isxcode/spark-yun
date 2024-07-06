156761e3c3ec80e144591d6a4c065c15e6e93c61
ispong <ispong@outlook.com>
2024-07-07 00:04:18 +0800

    修复环境参数提示

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/impl/ExecuteSqlExecutor.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/impl/ExecuteSqlExecutor.java
@@ -130,6 +130,8 @@ public class ExecuteSqlExecutor extends WorkExecutor {
 				logBuilder.append(LocalDateTime.now()).append(WorkLog.SUCCESS_INFO).append("SQL执行成功  \n");
 				workInstance = updateInstance(workInstance, logBuilder);
 			}
+		} catch (WorkRunException e) {
+			throw new WorkRunException(LocalDateTime.now() + WorkLog.ERROR_INFO + e.getMsg() + "\n");
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 			throw new WorkRunException(LocalDateTime.now() + WorkLog.ERROR_INFO + e.getMessage() + "\n");
