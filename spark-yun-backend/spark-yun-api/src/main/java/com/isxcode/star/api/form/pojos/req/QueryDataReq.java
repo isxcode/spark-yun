334c60a86ec2a53f7971662053c943cc0c5d3885
ispong <ispong@outlook.com>
2024-07-14 19:02:00 +0800

    修复计算引擎报错

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/form/pojos/req/QueryDataReq.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/form/pojos/req/QueryDataReq.java
@@ -3,10 +3,12 @@ package com.isxcode.star.api.form.pojos.req;
 import com.isxcode.star.backend.api.base.pojos.BasePageRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class QueryDataReq extends BasePageRequest {
 
 	@Schema(title = "表单唯一id", example = "sy_fd34e4a53db640f5943a4352c4d549b9")
