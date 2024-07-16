ede664aa6b929d69a35902dcb232b2126ca6768b
ispong <ispong@outlook.com>
2024-07-16 15:38:23 +0800

    集群名称不能重复

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/cluster/repository/ClusterRepository.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/cluster/repository/ClusterRepository.java
@@ -2,6 +2,7 @@ package com.isxcode.star.modules.cluster.repository;
 
 import com.isxcode.star.api.main.constants.ModuleCode;
 import com.isxcode.star.modules.cluster.entity.ClusterEntity;
+import jdk.nashorn.internal.runtime.options.Option;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +10,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @CacheConfig(cacheNames = {ModuleCode.CLUSTER})
@@ -18,4 +21,6 @@ public interface ClusterRepository extends JpaRepository<ClusterEntity, String> 
     Page<ClusterEntity> pageCluster(@Param("searchKeyWord") String searchKeyWord, Pageable pageable);
 
     long countByStatus(String status);
+
+    Optional<ClusterEntity> findByName(String name);
 }
