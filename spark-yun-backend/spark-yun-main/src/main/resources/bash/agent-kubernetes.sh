324734534ecd30154cb1163ff995ce8c9c92a100
ispong <ispong@outlook.com>
2024-07-17 11:24:34 +0800

    升级3.4.1版本

--- spark-yun-backend/spark-yun-main/src/main/resources/bash/agent-kubernetes.sh
+++ spark-yun-backend/spark-yun-main/src/main/resources/bash/agent-kubernetes.sh
@@ -127,7 +127,7 @@ if ! kubectl cluster-info &>/dev/null; then
 fi
 
 # 执行拉取spark镜像命令
-if ! docker image inspect apache/spark:3.4.1 &>/dev/null; then
+if ! docker image inspect spark:3.4.1 &>/dev/null; then
   json_output="{ \
             \"status\": \"INSTALL_ERROR\", \
             \"log\": \"没有spark:3.4.1镜像，需要执行拉取镜像命令，docker pull spark:3.4.1\" \
