beb2ed76e949d2ed08b4cef3fec267bd948e0c4e
lisc <2306210879@qq.com>
2024-07-25 02:50:36 +0800

    作业复制填充名称

--- spark-yun-frontend/src/views/workflow/workflow-page/copy-modal/index.vue
+++ spark-yun-frontend/src/views/workflow/workflow-page/copy-modal/index.vue
@@ -1,24 +1,11 @@
 <template>
-  <BlockModal :model-config="modelConfig">
-    <el-form
-      ref="form"
-      class="add-computer-group"
-      label-position="top"
-      :model="formData"
-      :rules="rules"
-    >
-      <el-form-item
-        label="名称"
-        prop="name"
-      >
-        <el-input
-          v-model="formData.name"
-          maxlength="200"
-          placeholder="请输入"
-        />
-      </el-form-item>
-    </el-form>
-  </BlockModal>
+    <BlockModal :model-config="modelConfig">
+        <el-form ref="form" class="add-computer-group" label-position="top" :model="formData" :rules="rules">
+            <el-form-item label="名称" prop="name">
+                <el-input v-model="formData.name" maxlength="200" placeholder="请输入" />
+            </el-form-item>
+        </el-form>
+    </BlockModal>
 </template>
 
 <script lang="ts" setup>
@@ -31,77 +18,80 @@ const callback = ref<any>()
 const renderSense = ref('')
 
 const modelConfig = reactive({
-  title: '复制作业',
-  visible: false,
-  width: '520px',
-  okConfig: {
-    title: '确定',
-    ok: okEvent,
-    disabled: false,
-    loading: false
-  },
-  cancelConfig: {
-    title: '取消',
-    cancel: closeEvent,
-    disabled: false
-  },
-  needScale: false,
-  zIndex: 1100,
-  closeOnClickModal: false
+    title: '复制作业',
+    visible: false,
+    width: '520px',
+    okConfig: {
+        title: '确定',
+        ok: okEvent,
+        disabled: false,
+        loading: false
+    },
+    cancelConfig: {
+        title: '取消',
+        cancel: closeEvent,
+        disabled: false
+    },
+    needScale: false,
+    zIndex: 1100,
+    closeOnClickModal: false
 })
 const formData = reactive({
-  name: '',
-  id: ''
+    name: '',
+    id: ''
 })
 const rules = reactive<FormRules>({
-  name: [{ required: true, message: '请输入作业名称', trigger: [ 'blur', 'change' ]}]
+    name: [{ required: true, message: '请输入作业名称', trigger: ['blur', 'change'] }]
 })
 
 function showModal(cb: () => void, data: any): void {
-  callback.value = cb
-  modelConfig.visible = true
-  formData.name = ''
-  formData.id = data.id
-  nextTick(() => {
-    form.value?.resetFields()
-  })
+    callback.value = cb
+    modelConfig.visible = true
+    formData.name = data.name + '_copy'
+    formData.id = data.id
+    nextTick(() => {
+        form.value?.resetFields()
+    })
 }
 
 function okEvent() {
-  form.value?.validate((valid) => {
-    if (valid) {
-      modelConfig.okConfig.loading = true
-      callback.value({
-        workName: formData.name,
-        workId: formData.id
-      }).then((res: any) => {
-        modelConfig.okConfig.loading = false
-        if (res === undefined) {
-          modelConfig.visible = false
+    form.value?.validate((valid) => {
+        if (valid) {
+            modelConfig.okConfig.loading = true
+            callback
+                .value({
+                    workName: formData.name,
+                    workId: formData.id
+                })
+                .then((res: any) => {
+                    modelConfig.okConfig.loading = false
+                    if (res === undefined) {
+                        modelConfig.visible = false
+                    } else {
+                        modelConfig.visible = true
+                    }
+                })
+                .catch((err: any) => {
+                    modelConfig.okConfig.loading = false
+                })
         } else {
-          modelConfig.visible = true
+            ElMessage.warning('请将表单输入完整')
         }
-      }).catch((err: any) => {
-        modelConfig.okConfig.loading = false
-      })
-    } else {
-      ElMessage.warning('请将表单输入完整')
-    }
-  })
+    })
 }
 
 function closeEvent() {
-  modelConfig.visible = false
+    modelConfig.visible = false
 }
 
 defineExpose({
-  showModal
+    showModal
 })
 </script>
 
 <style lang="scss">
 .add-computer-group {
-  padding: 12px 20px 0 20px;
-  box-sizing: border-box;
+    padding: 12px 20px 0 20px;
+    box-sizing: border-box;
 }
 </style>
