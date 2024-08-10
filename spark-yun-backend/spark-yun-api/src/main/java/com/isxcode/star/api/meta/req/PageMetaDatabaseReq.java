d8fbd4018797cff86c95d9b43291cb1842dc103f
ispong <ispong@outlook.com>
2024-08-10 18:26:54 +0800

    元数据采集

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/meta/req/PageMetaDatabaseReq.java
@@ -0,0 +1,12 @@
+package com.isxcode.star.api.meta.req;
+
+import com.isxcode.star.backend.api.base.pojos.BasePageRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class PageMetaDatabaseReq extends BasePageRequest {
+
+}
