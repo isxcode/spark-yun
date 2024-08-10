0dbb6930132c0d3f991f0da475ec58322c36163b
ispong <ispong@outlook.com>
2024-08-10 18:30:20 +0800

    代码格式化

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/meta/req/PageMetaTableReq.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/meta/req/PageMetaTableReq.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.validation.constraints.NotEmpty;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
