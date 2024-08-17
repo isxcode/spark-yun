5089f028f3604d6ef42b76f65d169b5992b992c8
ispong <ispong@outlook.com>
2024-08-17 20:10:09 +0800

    优化采集代码

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/datasource/source/impl/MysqlService.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/datasource/source/impl/MysqlService.java
@@ -2,7 +2,8 @@ package com.isxcode.star.modules.datasource.source.impl;
 
 import com.isxcode.star.api.datasource.constants.DatasourceDriver;
 import com.isxcode.star.api.datasource.constants.DatasourceType;
-import com.isxcode.star.api.datasource.pojos.dto.QueryTablesDto;
+import com.isxcode.star.api.datasource.pojos.dto.QueryColumnDto;
+import com.isxcode.star.api.datasource.pojos.dto.QueryTableDto;
 import com.isxcode.star.backend.api.base.properties.IsxAppProperties;
 import com.isxcode.star.common.utils.AesUtils;
 import com.isxcode.star.modules.datasource.service.DatabaseDriverService;
@@ -10,7 +11,6 @@ import com.isxcode.star.modules.datasource.source.Datasource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
@@ -21,12 +21,8 @@ import java.util.List;
 @Slf4j
 public class MysqlService extends Datasource {
 
-    private final JdbcTemplate jdbcTemplate;
-
-    public MysqlService(DatabaseDriverService dataDriverService, IsxAppProperties isxAppProperties, AesUtils aesUtils,
-        JdbcTemplate jdbcTemplate) {
+    public MysqlService(DatabaseDriverService dataDriverService, IsxAppProperties isxAppProperties, AesUtils aesUtils) {
         super(dataDriverService, isxAppProperties, aesUtils);
-        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
@@ -40,13 +36,24 @@ public class MysqlService extends Datasource {
     }
 
     @Override
-    protected List<QueryTablesDto> queryTables(Connection connection, String database) throws SQLException {
-
+    protected List<QueryTableDto> queryTable(Connection connection, String database) throws SQLException {
         QueryRunner qr = new QueryRunner();
-        List<QueryTablesDto> query = qr.query(connection,
+        List<QueryTableDto> query = qr.query(connection,
             "SELECT tables.table_name as tableName FROM information_schema.tables WHERE table_schema = '" + database
                 + "'",
-            new BeanListHandler<>(QueryTablesDto.class));
+            new BeanListHandler<>(QueryTableDto.class));
+        connection.close();
+        return query;
+    }
+
+    @Override
+    protected List<QueryColumnDto> queryColumn(Connection connection, String database, String tableName)
+        throws SQLException {
+        QueryRunner qr = new QueryRunner();
+        List<QueryColumnDto> query = qr.query(connection,
+            "SELECT COLUMN_NAME as columnName,DATA_TYPE as columnType FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '"
+                + database + "' AND TABLE_NAME = '" + tableName + "'",
+            new BeanListHandler<>(QueryColumnDto.class));
         connection.close();
         return query;
     }
