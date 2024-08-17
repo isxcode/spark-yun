c9605cda63508b6a4a991e5171300edc99113121
ispong <ispong@outlook.com>
2024-08-17 20:10:12 +0800

    优化采集代码

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/datasource/pojos/dto/QueryColumnDto.java
@@ -0,0 +1,15 @@
+package com.isxcode.star.api.datasource.pojos.dto;
+
+import lombok.Data;
+
+@Data
+public class QueryColumnDto {
+
+    private String datasourceId;
+
+    private String tableName;
+
+    private String columnName;
+
+    private String columnType;
+}
