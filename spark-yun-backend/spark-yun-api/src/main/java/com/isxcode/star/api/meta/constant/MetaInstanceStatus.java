f65d945eb919695154eea6e83603164c011b63e4
ispong <ispong@outlook.com>
2024-08-19 11:34:36 +0800

    采集实例查询

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/meta/constant/MetaInstanceStatus.java
@@ -0,0 +1,24 @@
+package com.isxcode.star.api.meta.constant;
+
+public interface MetaInstanceStatus {
+
+    /**
+     * 采集中.
+     */
+    String COLLECTING = "COLLECTING";
+
+    /**
+     * 失败.
+     */
+    String FAIL = "FAIL";
+
+    /**
+     * 成功.
+     */
+    String SUCCESS = "SUCCESS";
+
+    /**
+     * 已中止.
+     */
+    String ABORT = "ABORT";
+}
