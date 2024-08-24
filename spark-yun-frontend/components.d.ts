954685276a9874dedac8db67161b6528582c1d79
ispong <ispong@outlook.com>
2024-08-24 18:24:57 +0800

    优化官方文档

--- spark-yun-frontend/components.d.ts
+++ spark-yun-frontend/components.d.ts
@@ -49,7 +49,6 @@ declare module 'vue' {
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElStep: typeof import('element-plus/es')['ElStep']
     ElSteps: typeof import('element-plus/es')['ElSteps']
-    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
