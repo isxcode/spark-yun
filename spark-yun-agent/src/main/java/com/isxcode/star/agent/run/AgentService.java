cb349fd667c20319a4203b879923548646ab5b3a
ispong <ispong@outlook.com>
2024-08-27 17:22:31 +0800

    代理的controller中，禁止抛出异常

--- spark-yun-agent/src/main/java/com/isxcode/star/agent/run/AgentService.java
+++ spark-yun-agent/src/main/java/com/isxcode/star/agent/run/AgentService.java
@@ -8,26 +8,53 @@ import java.io.IOException;
 
 public interface AgentService {
 
+    /**
+     * 获取spark提交的master信息.
+     */
     String getMaster(String sparkHomePath) throws IOException;
 
     /**
-     * @param yagExecuteWorkReq 请求体
+     * 初始化spark提交的SparkLauncher.
      */
     SparkLauncher genSparkLauncher(YagExecuteWorkReq yagExecuteWorkReq) throws IOException;
 
+    /**
+     * 执行spark作业.
+     */
     String executeWork(SparkLauncher sparkLauncher) throws IOException;
 
+    /**
+     * 获取spark作业的状态.
+     */
     String getAppStatus(String appId, String sparkHomePath) throws IOException;
 
+    /**
+     * 获取spark作业的运行日志.
+     */
     String getAppLog(String appId, String sparkHomePath) throws IOException;
 
+    /**
+     * 获取spark作业的输出日志.
+     */
     String getStdoutLog(String appId, String sparkHomePath) throws IOException;
 
+    /**
+     * 获取spark作业返回的数据.
+     */
     String getAppData(String appId, String sparkHomePath) throws IOException;
 
+    /**
+     * 中止spark作业.
+     */
     void killApp(String appId, String sparkHomePath, String agentHomePath) throws IOException;
 
+    /**
+     * 获取当前代理的类型.
+     */
     String getAgentName();
 
+    /**
+     * 获取容器话部署的SparkLauncher.
+     */
     SparkLauncher genSparkLauncher(DeployContainerReq deployContainerReq) throws IOException;
 }
