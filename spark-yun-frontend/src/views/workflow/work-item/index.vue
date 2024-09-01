8aac59dee8b7ed6ac2ca469d06d8d626494ace1b
fancinate <1585546519@qq.com>
2024-09-01 13:50:56 +0800

    优化作业流作业输入框&日志展示优化

--- spark-yun-frontend/src/views/workflow/work-item/index.vue
+++ spark-yun-frontend/src/views/workflow/work-item/index.vue
@@ -68,16 +68,32 @@
               <span class="btn-text">定位</span>
             </div>
           </div>
-          <code-mirror v-model="sqltextData" basic :lang="workConfig.workType === 'PYTHON' ? pythonLang : lang" @change="sqlConfigChange"/>
+          <code-mirror v-model="sqltextData" basic :lang="workConfig.workType === 'PYTHON' ? pythonLang : lang"
+            @change="sqlConfigChange" />
         </div>
-        <div class="log-show">
-          <el-tabs v-model="activeName" @tab-change="tabChangeEvent">
-            <template v-for="tab in tabList" :key="tab.code">
-              <el-tab-pane v-if="!tab.hide" :label="tab.name" :name="tab.code" />
+
+        <el-collapse v-model="collapseActive" class="work-item-log__collapse" ref="logCollapseRef">
+          <el-collapse-item title="查看日志" :disabled="true" name="1">
+            <template #title>
+              <el-tabs v-model="activeName" @tab-click="changeCollapseUp" @tab-change="tabChangeEvent">
+                <template v-for="tab in tabList" :key="tab.code">
+                  <el-tab-pane v-if="!tab.hide" :label="tab.name" :name="tab.code" />
+                </template>
+              </el-tabs>
+              <span class="log__collapse">
+                <el-icon v-if="isCollapse" @click="changeCollapseDown">
+                  <ArrowDown />
+                </el-icon>
+                <el-icon v-else @click="changeCollapseUp">
+                  <ArrowUp />
+                </el-icon>
+              </span>
             </template>
-          </el-tabs>
-          <component :is="currentTab" ref="containerInstanceRef" class="show-container" />
-        </div>
+            <div class="log-show log-show-datasync">
+              <component :is="currentTab" ref="containerInstanceRef" class="show-container" />
+            </div>
+          </el-collapse-item>
+        </el-collapse>
       </div>
     </LoadingPage>
     <!-- <ConfigModal ref="configModalRef" /> -->
@@ -90,13 +106,11 @@
 import { reactive, ref, onMounted, markRaw, nextTick } from 'vue'
 import Breadcrumb from '@/layout/bread-crumb/index.vue'
 import LoadingPage from '@/components/loading/index.vue'
-// import ConfigModal from './config-modal/index.vue'
 import ConfigDetail from '../workflow-page/config-detail/index.vue'
 import PublishLog from './publish-log.vue'
 import ReturnData from './return-data.vue'
 import RunningLog from './running-log.vue'
 import TotalDetail from './total-detail.vue'
-// import CodeMirror from 'vue-codemirror6'
 import { sql } from '@codemirror/lang-sql'
 import { python } from '@codemirror/lang-python'
 
@@ -132,6 +146,10 @@ const instanceId = ref('')
 const changeStatus = ref(false)
 
 const containerInstanceRef = ref(null)
+
+const logCollapseRef = ref()
+const collapseActive = ref('0')
+const isCollapse = ref(false)
 
 let workConfig = reactive({
   clusterId: '',
@@ -195,7 +213,7 @@ function initData(id?: string, tableLoading?: boolean) {
                   item.hide = status === 'FAIL' ? true : false
                 }
               })
-            } else if (['QUERY_JDBC', 'SPARK_CONTAINER_SQL','PRQL'].includes(workConfig.workType)) {
+            } else if (['QUERY_JDBC', 'SPARK_CONTAINER_SQL', 'PRQL'].includes(workConfig.workType)) {
               tabList.forEach((item: any) => {
                 if (['ReturnData'].includes(item.code)) {
                   item.hide = status === 'FAIL' ? true : false
@@ -287,6 +305,9 @@ function runWorkData() {
           instanceId.value = res.data.instanceId
           ElMessage.success(res.msg)
           initData(res.data.instanceId, true)
+          nextTick(() => {
+            changeCollapseUp()
+          })
         })
         .catch(() => {
           runningLoading.value = false
@@ -311,6 +332,9 @@ function runWorkData() {
         // 点击运行，默认跳转到提交日志tab
         activeName.value = 'PublishLog'
         currentTab.value = markRaw(PublishLog)
+        nextTick(() => {
+          changeCollapseUp()
+        })
       })
       .catch(() => {
         runningLoading.value = false
@@ -369,9 +393,9 @@ function publishData() {
     ElMessage.success(res.msg)
     publishLoading.value = false
   })
-  .catch((error: any) => {
-    publishLoading.value = false
-  })
+    .catch((error: any) => {
+      publishLoading.value = false
+    })
 }
 
 // 下线
@@ -383,9 +407,9 @@ function stopData() {
     ElMessage.success(res.msg)
     stopLoading.value = false
   })
-  .catch((error: any) => {
-    stopLoading.value = false
-  })
+    .catch((error: any) => {
+      stopLoading.value = false
+    })
 }
 
 // 配置打开
@@ -393,6 +417,14 @@ function setConfigData() {
   configDetailRef.value.showModal(props.workItemConfig, () => {
     initData()
   })
+}
+function changeCollapseDown() {
+  logCollapseRef.value.setActiveNames('0')
+  isCollapse.value = false
+}
+function changeCollapseUp() {
+  logCollapseRef.value.setActiveNames('1')
+  isCollapse.value = true
 }
 
 function sqlConfigChange(e: string) {
@@ -417,13 +449,13 @@ onMounted(() => {
   .zqy-work-container {
     .sql-code-container {
       .vue-codemirror {
-        // height: calc(100vh - 544px);
-        height: 190px;
+        height: calc(100vh - 174px);
         .cm-editor {
           height: 100%;
           outline: none;
           border: 1px solid #dcdfe6;
         }
+
         .cm-gutters {
           font-size: 12px;
           font-family: v-sans, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
@@ -481,27 +513,53 @@ onMounted(() => {
           }
 
           &:hover {
-            color: getCssVar('color', 'primary');;
+            color: getCssVar('color', 'primary');
+            ;
           }
         }
       }
 
-      .el-textarea {
-        .el-textarea__inner {
-          border-radius: getCssVar('border-radius', 'small');
-          font-size: getCssVar('font-size', 'extra-small');
-        }
-      }
     }
 
-    .log-show {
-      position: relative;
-      .zqy-download-log {
-        top: 13px;
+    .work-item-log__collapse {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: -2px;
+      z-index: 100;
+
+      .el-collapse-item__header {
+        // padding-left: 20px;
+        cursor: default;
       }
+
+      .el-collapse-item__arrow {
+        display: none;
+      }
+
+      .el-collapse-item__content {
+        padding-bottom: 14px;
+      }
+
+      .log__collapse {
+        position: absolute;
+        right: 20px;
+        cursor: pointer;
+      }
+
       .el-tabs {
+        width: 100%;
+        // padding: 0 20px;
+        height: 40px;
+        box-sizing: border-box;
+
         .el-tabs__item {
           font-size: getCssVar('font-size', 'extra-small');
+        }
+
+        .el-tabs__nav-scroll {
+          padding-left: 20px;
+          box-sizing: border-box;
         }
 
         .el-tabs__content {
@@ -513,9 +571,30 @@ onMounted(() => {
         }
       }
 
-      .show-container {
-        height: calc(100vh - 368px);
-        overflow: auto;
+      .log-show {
+        padding: 0 20px;
+        box-sizing: border-box;
+
+        &.log-show-datasync {
+          height: calc(100vh - 368px);
+          .zqy-download-log {
+            right: 40px;
+            top: 12px;
+          }
+        }
+
+        pre {
+          width: 100px;
+        }
+
+        .show-container {
+          height: calc(100vh - 368px);
+          overflow: auto;
+        }
+
+        .empty-page {
+          height: 80%;
+        }
       }
     }
   }
