55b0497b124531dd8e75a980fa86489796b9234b
ispong <ispong@outlook.com>
2024-10-02 22:52:45 +0800

    优化官网

--- docs/components/layout/home-footer.vue
+++ docs/components/layout/home-footer.vue
@@ -83,14 +83,14 @@
 <style lang="scss" scoped>
 
 @font-face {
-  font-family: "阿里妈妈数黑体 Bold";font-weight: 700;src: url("//at.alicdn.com/wf/webfont/UMV2yX61q8rB/fxITmChMPbzu.woff2") format("woff2"),
-  url("//at.alicdn.com/wf/webfont/UMV2yX61q8rB/LJtJgtgpkS9F.woff") format("woff");
+  font-family: "阿里妈妈数黑体 Bold";font-weight: 700;src: url("//at.alicdn.com/wf/webfont/UMV2yX61q8rB/riiNKOyVZ6o8.woff2") format("woff2"),
+  url("//at.alicdn.com/wf/webfont/UMV2yX61q8rB/uMY8BRRFRukh.woff") format("woff");
   font-display: swap;
 }
 
 @font-face {
-  font-family: "阿里巴巴普惠体 2.0 45 Light";font-weight: 300;src: url("//at.alicdn.com/wf/webfont/UMV2yX61q8rB/ZDyJM2ttTReK.woff2") format("woff2"),
-  url("//at.alicdn.com/wf/webfont/UMV2yX61q8rB/pkAPEWm8dB4R.woff") format("woff");
+  font-family: "阿里巴巴普惠体 2.0 45 Light";font-weight: 300;src: url("//at.alicdn.com/wf/webfont/UMV2yX61q8rB/B4Wux4WUE9wD.woff2") format("woff2"),
+  url("//at.alicdn.com/wf/webfont/UMV2yX61q8rB/bLnzv8Ey29iP.woff") format("woff");
   font-display: swap;
 }
 
