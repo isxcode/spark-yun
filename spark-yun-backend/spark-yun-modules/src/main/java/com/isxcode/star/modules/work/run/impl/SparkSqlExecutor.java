3c4d2c0a5893acbe77d098c3af714dcda9f6515f
ispong <ispong@outlook.com>
2024-10-19 12:09:02 +0800

    优化jar包话术

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/impl/SparkSqlExecutor.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/impl/SparkSqlExecutor.java
@@ -232,7 +232,8 @@ public class SparkSqlExecutor extends WorkExecutor {
                         engineNode.getAgentHomePath() + "/zhiqingyun-agent/file/" + e.getFileId() + ".jar");
                 } catch (JSchException | SftpException | InterruptedException | IOException ex) {
                     log.error(ex.getMessage(), ex);
-                    throw new WorkRunException(LocalDateTime.now() + WorkLog.ERROR_INFO + "自定义函数jar文件上传失败，请检查文件是否上传或者重新上传\n");
+                    throw new WorkRunException(
+                        LocalDateTime.now() + WorkLog.ERROR_INFO + "自定义函数jar文件上传失败，请检查文件是否上传或者重新上传\n");
                 }
             });
             pluginReq.setFuncInfoList(funcMapper.funcEntityListToFuncInfoList(allFunc));
@@ -248,7 +249,8 @@ public class SparkSqlExecutor extends WorkExecutor {
                         engineNode.getAgentHomePath() + "/zhiqingyun-agent/file/" + e.getId() + ".jar");
                 } catch (JSchException | SftpException | InterruptedException | IOException ex) {
                     log.error(ex.getMessage(), ex);
-                    throw new WorkRunException(LocalDateTime.now() + WorkLog.ERROR_INFO + "自定义依赖jar文件上传失败，请检查文件是否上传或者重新上传\n");
+                    throw new WorkRunException(
+                        LocalDateTime.now() + WorkLog.ERROR_INFO + "自定义依赖jar文件上传失败，请检查文件是否上传或者重新上传\n");
                 }
             });
             executeReq.setLibConfig(workRunContext.getLibConfig());
