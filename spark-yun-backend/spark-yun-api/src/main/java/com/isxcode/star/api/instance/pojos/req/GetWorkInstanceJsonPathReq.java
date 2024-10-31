f63b9e5ecb6a63e0b2728500b6fae35e13ba8d08
ispong <ispong@outlook.com>
2024-10-31 18:36:58 +0800

    修改话术

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/instance/pojos/req/GetWorkInstanceJsonPathReq.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/instance/pojos/req/GetWorkInstanceJsonPathReq.java
@@ -7,6 +7,6 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class GetWorkInstanceJsonPathReq {
 
-    @NotEmpty(message = "作业流id不能为空")
+    @NotEmpty(message = "作业实例id不能为空")
     private String workInstanceId;
 }
