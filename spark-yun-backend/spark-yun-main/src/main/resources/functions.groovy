2740f0c125c30b02a0cb6534be0eea6be5aea8a8
ispong <ispong@outlook.com>
2024-11-01 14:33:42 +0800

    支持正则解析结果集

--- spark-yun-backend/spark-yun-main/src/main/resources/functions.groovy
+++ spark-yun-backend/spark-yun-main/src/main/resources/functions.groovy
@@ -1,7 +1,10 @@
 import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.TypeReference
 import com.alibaba.fastjson2.JSONPath
 
 import java.text.SimpleDateFormat
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 def Date now() {
 
@@ -34,7 +37,30 @@ def String get_json_value(String jsonString, String jsonPath) {
 
     try {
         return String.valueOf(JSONPath.eval(JSON.parse(new String(Base64.getDecoder().decode(jsonString))), jsonPath))
+    } catch (Exception ignored) {
+        return '#[[get_json_value(\'' + jsonString + '\',\'' + jsonPath + '\')]]'
+    }
+}
+
+def String get_regex_value(String strValue, String patternStr) {
+
+    try {
+        Pattern pattern = Pattern.compile(patternStr);
+        Matcher matcher = pattern.matcher(new String(Base64.getDecoder().decode(strValue)))
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+    } catch (Exception ignored) {
+        return '#[[get_regex_value(\'' + strValue + '\',\'' + patternStr + '\')]]'
+    }
+}
+
+def String get_table_value(String dataStr, Integer row, Integer col) {
+
+    try {
+        List<List<String>> data = JSON.parseObject(new String(Base64.getDecoder().decode(dataStr)), new TypeReference<List<List<String>>>() {});
+        return data[row][col - 1]
     } catch (Exception e) {
-        return '#[get_json_value(\'' + jsonString + '\',\'' + jsonPath + '\')]'
+        return '#[[get_table_value(\'' + dataStr + '\',' + row + ',' + col + ')]]'
     }
 }
