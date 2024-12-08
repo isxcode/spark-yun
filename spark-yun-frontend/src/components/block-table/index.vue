ba18e40c71f3c4c5451f323f643552751d947bcf
ispong <ispong@outlook.com>
2024-12-08 22:59:16 +0800

    GH-1397 [Improve] 数据预览优化 (#1401)
    Co-authored-by: fancinate <1585546519@qq.com>

--- spark-yun-frontend/src/components/block-table/index.vue
+++ spark-yun-frontend/src/components/block-table/index.vue
@@ -21,6 +21,7 @@
         :width="colConfig.width"
         :field="colConfig.prop"
         :fixed="colConfig.fixed"
+        :resizable="colIndex < tableConfig.colConfigs.length - 1"
         :show-header-overflow="colConfig.showHeaderOverflow || false"
         :show-overflow="colConfig.showOverflowTooltip || false"
         v-bind="colConfig"
@@ -41,6 +42,7 @@
         :show-header-overflow="colConfig.showHeaderOverflow || false"
         :width="colConfig.width"
         :field="colConfig.prop"
+        :resizable="colIndex < tableConfig.colConfigs.length - 1"
         :show-overflow="colConfig.showOverflowTooltip || false"
         v-bind="colConfig"
       />
@@ -67,8 +69,9 @@
 </template>
 
 <script lang="ts" setup>
-import { defineProps, defineEmits } from 'vue'
+import { defineProps, defineEmits, reactive } from 'vue'
 import EmptyPage from '@/components/empty-page/index.vue'
+import { VxeTablePropTypes } from 'vxe-table'
 
 interface Pagination {
   currentPage: number;
@@ -109,10 +112,10 @@ const handleCurrentChange = (e: number) => {
 }
 
 function seqMethod({ rowIndex }):number {
-  if (props.tableConfig) {
+  if (props.tableConfig && props.tableConfig.pagination) {
     return (props.tableConfig?.pagination.currentPage - 1) * props.tableConfig.pagination.pageSize + rowIndex + 1
   } else {
-    return rowIndex
+    return rowIndex + 1
   }
 }
 
