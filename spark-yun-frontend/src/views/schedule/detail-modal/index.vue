759fc1885dd2d12bd739580da6b507ae33ee3625
fancinate <1585546519@qq.com>
2024-12-12 21:51:55 +0800

    调度-curl作业运行接口查看适配&弹窗loading适配

--- spark-yun-frontend/src/views/schedule/detail-modal/index.vue
+++ spark-yun-frontend/src/views/schedule/detail-modal/index.vue
@@ -1,24 +1,24 @@
 <template>
-  <BlockModal
-    :model-config="modelConfig"
-    @close="closeEvent"
-  >
-    <div
-      id="content"
-      class="content-box"
-    >
-      <!-- 日志展示 -->
-      <template v-if="['log', 'yarnLog'].includes(modalType)">
-        <LogContainer v-if="logMsg" :logMsg="logMsg" :status="true"></LogContainer>
-        <template v-else>
-          <empty-page label="暂无日志"></empty-page>
+  <BlockModal :model-config="modelConfig" @close="closeEvent">
+    <LoadingPage class="log-loading" :visible="loading">
+      <div id="content" class="content-box">
+        <!-- 日志展示 -->
+        <template v-if="['log', 'yarnLog'].includes(modalType)">
+          <LogContainer v-if="logMsg" :logMsg="logMsg" :status="true"></LogContainer>
+          <template v-else>
+            <empty-page label="暂无日志"></empty-page>
+          </template>
         </template>
-      </template>
-      <!-- 结果展示 -->
-      <template v-else-if="modalType === 'result'">
-        <BlockTable :table-config="tableConfig" />
-      </template>
-    </div>
+        <!-- 结果展示 -->
+        <template v-else-if="modalType === 'result'">
+          <LogContainer v-if="strData || jsonData" :logMsg="strData || jsonData" :showResult="true" :status="true">
+          </LogContainer>
+          <template v-else>
+            <BlockTable :table-config="tableConfig" />
+          </template>
+        </template>
+      </div>
+    </LoadingPage>
   </BlockModal>
 </template>
 
@@ -26,7 +26,7 @@
 import { reactive, defineExpose, ref, nextTick, onUnmounted } from 'vue'
 import BlockModal from '@/components/block-modal/index.vue'
 import BlockTable from '@/components/block-table/index.vue'
-
+import LoadingPage from '@/components/loading/index.vue'
 import { GetLogData, GetYarnLogData, GetResultData } from '@/services/schedule.service'
 
 const callback = ref<any>()
@@ -34,6 +34,10 @@ const logMsg = ref('')
 const info = ref('')
 const modalType = ref('')
 const timer = ref(null)
+
+const jsonData = ref()
+const strData = ref()
+const loading = ref<boolean>(false)
 
 const tableConfig = reactive({
   tableData: [],
@@ -52,7 +56,7 @@ const modelConfig = reactive({
   },
   needScale: false,
   zIndex: 1100,
-  customClass: 'zqy-log-modal',
+  customClass: 'zqy-log-modal zqy-log-modal__s',
   closeOnClickModal: false
 })
 
@@ -61,7 +65,14 @@ function showModal(cb: () => void, data: any, type: string): void {
   info.value = data.id
   modalType.value = type
 
+  logMsg.value = ''
+  jsonData.value = null
+  strData.value = null
+  tableConfig.colConfigs = []
+  tableConfig.tableData = []
+
   if (modalType.value === 'log') {
+    loading.value = true
     getLogData()
     if (!timer.value) {
       timer.value = setInterval(() => {
@@ -86,33 +97,44 @@ function getLogData() {
     instanceId: info.value
   })
     .then((res: any) => {
+      loading.value = false
       logMsg.value = res.data.log
     })
     .catch(() => {
       logMsg.value = ''
+      loading.value = false
     })
 }
 
 // 获取yarn日志
 function getYarnLogData() {
+  loading.value = true
   GetYarnLogData({
     instanceId: info.value
   })
     .then((res: any) => {
+      loading.value = false
       logMsg.value = res.data.yarnLog
     })
     .catch(() => {
+      loading.value = false
       logMsg.value = ''
     })
 }
 
 // 获取结果
 function getResultDatalist() {
+  loading.value = true
   tableConfig.loading = true
   GetResultData({
     instanceId: info.value
   })
     .then((res: any) => {
+      jsonData.value = res.data.jsonData
+      strData.value = res.data.strData
+
+      loading.value = false
+
       const col = res.data.data.slice(0, 1)[0]
       const tableData = res.data.data.slice(1, res.data.data.length)
       tableConfig.colConfigs = col.map((colunm: any) => {
@@ -138,6 +160,7 @@ function getResultDatalist() {
       tableConfig.colConfigs = []
       tableConfig.tableData = []
       tableConfig.loading = false
+      loading.value = false
     })
 }
 
@@ -160,3 +183,11 @@ defineExpose({
   showModal
 })
 </script>
+
+<style lang="scss">
+.zqy-log-modal__s {
+  .content-box {
+    position: static;
+  }
+}
+</style>
