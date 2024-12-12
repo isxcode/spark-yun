a494a5750800c33e7473318dca66db4b01fb999c
ispong <ispong@outlook.com>
2024-12-12 18:56:55 +0800

    GH-1423 [Feature] 元数据兼容postgresql (#1436)
    * Postgresql元数据采集方法初版完成
    
    * 格式化代码
    
    ---------
    
    Co-authored-by: Forzh <hao.zhang5@definesys.com>

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/datasource/source/impl/PostgresService.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/datasource/source/impl/PostgresService.java
@@ -12,9 +12,15 @@ import com.isxcode.star.common.utils.AesUtils;
 import com.isxcode.star.modules.datasource.service.DatabaseDriverService;
 import com.isxcode.star.modules.datasource.source.Datasource;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 @Service
@@ -38,27 +44,94 @@ public class PostgresService extends Datasource {
 
     @Override
     public List<QueryTableDto> queryTable(ConnectInfo connectInfo) throws IsxAppException {
-        throw new RuntimeException("该数据源暂不支持");
+
+        QueryRunner qr = new QueryRunner();
+        try (Connection connection = getConnection(connectInfo)) {
+
+            String sql = "SELECT '" + connectInfo.getDatasourceId() + "' AS datasourceId, t.tablename AS tableName, "
+                + "obj_description(format('%s.%s', t.schemaname, t.tablename)::regclass, 'pg_class') AS tableComment "
+                + "FROM pg_catalog.pg_tables t WHERE t.schemaname = 'public'";
+
+            if (Strings.isNotEmpty(connectInfo.getTablePattern())) {
+                sql += " AND tablename ~ '" + connectInfo.getTablePattern() + "'";
+                return qr.query(connection, sql, new BeanListHandler<>(QueryTableDto.class));
+            } else {
+                return qr.query(connection, sql, new BeanListHandler<>(QueryTableDto.class));
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new IsxAppException(e.getMessage());
+        }
     }
 
     @Override
     public List<QueryColumnDto> queryColumn(ConnectInfo connectInfo) throws IsxAppException {
-        throw new RuntimeException("该数据源暂不支持");
+
+        Assert.notNull(connectInfo.getTableName(), "tableName不能为空");
+
+        // 设置schema a.attrelid = 'schema.tablename'::regclass
+        QueryRunner qr = new QueryRunner();
+        try (Connection connection = getConnection(connectInfo)) {
+            return qr.query(connection, "SELECT '" + connectInfo.getDatasourceId() + "' as datasourceId, '"
+                + connectInfo.getTableName() + "' AS tableName, a.attname AS columnName, false as isPartitionColumn, "
+                + "pg_catalog.format_type(a.atttypid, a.atttypmod) AS columnType, col_description(a.attrelid, a.attnum) AS columnComment "
+                + "FROM pg_catalog.pg_attribute a WHERE a.attrelid = '" + connectInfo.getTableName()
+                + "'::regclass AND a.attnum > 0 AND NOT a.attisdropped", new BeanListHandler<>(QueryColumnDto.class));
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new IsxAppException(e.getMessage());
+        }
     }
 
     @Override
     public Long getTableTotalSize(ConnectInfo connectInfo) throws IsxAppException {
-        return 0L;
+
+        Assert.notNull(connectInfo.getTableName(), "tableName不能为空");
+
+        QueryRunner qr = new QueryRunner();
+        try (Connection connection = getConnection(connectInfo)) {
+            Object query =
+                qr.query(connection, "SELECT pg_total_relation_size('" + connectInfo.getTableName() + "'::regclass);",
+                    new ScalarHandler<>());
+            return Long.parseLong(String.valueOf(query));
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new IsxAppException(e.getMessage());
+        }
     }
 
     @Override
     public Long getTableTotalRows(ConnectInfo connectInfo) throws IsxAppException {
-        return 0L;
+
+        Assert.notNull(connectInfo.getTableName(), "tableName不能为空");
+
+        QueryRunner qr = new QueryRunner();
+        try (Connection connection = getConnection(connectInfo)) {
+            Object query =
+                qr.query(connection, "SELECT count(*) FROM " + connectInfo.getTableName(), new ScalarHandler<>());
+            return Long.valueOf(String.valueOf(query));
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new IsxAppException(e.getMessage());
+        }
     }
 
     @Override
     public Long getTableColumnCount(ConnectInfo connectInfo) throws IsxAppException {
-        return 0L;
+
+        Assert.notNull(connectInfo.getTableName(), "tableName不能为空");
+
+        QueryRunner qr = new QueryRunner();
+        try (Connection connection = getConnection(connectInfo)) {
+            Object query = qr.query(
+                connection, "SELECT COUNT(*) FROM pg_catalog.pg_attribute " + "WHERE attrelid = '"
+                    + connectInfo.getTableName() + "'::regclass AND attnum > 0 AND NOT attisdropped",
+                new ScalarHandler<>());
+            return Long.valueOf(String.valueOf(query));
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new IsxAppException(e.getMessage());
+        }
     }
 
     @Override
