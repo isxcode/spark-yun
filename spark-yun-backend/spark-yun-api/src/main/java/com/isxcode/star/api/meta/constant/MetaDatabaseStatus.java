42deebefe2e87ee17e675675e7a976cc3dcaeef5
ispong <ispong@outlook.com>
2025-01-02 18:23:51 +0800

    数据源添加删除判断

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/meta/constant/MetaDatabaseStatus.java
@@ -0,0 +1,14 @@
+package com.isxcode.star.api.meta.constant;
+
+public interface MetaDatabaseStatus {
+
+    /**
+     * 已删除.
+     */
+    String DELETED = "DELETED";
+
+    /**
+     * 未删除.
+     */
+    String ACTIVE = "ACTIVE";
+}
