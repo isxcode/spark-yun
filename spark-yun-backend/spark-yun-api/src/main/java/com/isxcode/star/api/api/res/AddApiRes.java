69f273e321deea9d96d3876ab322ef168b4853b8
ispong <ispong@outlook.com>
2025-02-21 10:32:58 +0800

    扩充自定义接口返回id

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/api/res/AddApiRes.java
@@ -0,0 +1,11 @@
+package com.isxcode.star.api.api.res;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AddApiRes {
+
+    private String id;
+}
---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/api/res/UpdateApiRes.java
@@ -0,0 +1,11 @@
+package com.isxcode.star.api.api.res;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UpdateApiRes {
+
+    private String id;
+}
