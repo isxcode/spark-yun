bd1ea969c71612f201fce0cc6ee945121a246010
ispong <ispong@outlook.com>
2025-03-21 17:57:27 +0800

    新增单点登录配置接口

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/auth/constants/AuthStatus.java
@@ -0,0 +1,14 @@
+package com.isxcode.star.api.auth.constants;
+
+public interface AuthStatus {
+
+    /**
+     * 启用.
+     */
+    String ENABLE = "ENABLE";
+
+    /**
+     * 禁用.
+     */
+    String DISABLE = "DISABLE";
+}
