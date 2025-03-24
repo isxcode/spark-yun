8132ecf9a2a5687a20bf4aab3b77b5dbefd3f0af
ispong <ispong@outlook.com>
2025-03-24 11:25:21 +0800

    修复sql执行异常

--- spark-yun-backend/spark-yun-main/src/main/resources/db/migration/postgres/V30__GH-1597.sql
+++ spark-yun-backend/spark-yun-main/src/main/resources/db/migration/postgres/V30__GH-1597.sql
@@ -11,11 +11,11 @@ INSERT INTO SY_DATABASE_DRIVER (id, name, db_type, file_name, driver_type, creat
                                 is_default_driver)
 VALUES ('gbase8a', 'gbase8a', 'GBASE', 'gbase-connector-java-9.5.0.7-build1-bin.jar',
         'SYSTEM_DRIVER', 'zhiqingyun', '2023-11-01 16:54:34', 'zhiqingyun', '2023-11-01 16:54:39', 1, 0, 'zhiqingyun',
-        '系统自带驱动', 1);
+        '系统自带驱动', true);
 -- 添加sybase
 INSERT INTO SY_DATABASE_DRIVER (id, name, db_type, file_name, driver_type, create_by, create_date_time,
                                 last_modified_by, last_modified_date_time, version_number, deleted, tenant_id, remark,
                                 is_default_driver)
 VALUES ('sybase', 'sybase', 'SYBASE', 'jconn4-16.0.jar',
         'SYSTEM_DRIVER', 'zhiqingyun', '2023-11-01 16:54:34', 'zhiqingyun', '2023-11-01 16:54:39', 1, 0, 'zhiqingyun',
-        '系统自带驱动', 1);
+        '系统自带驱动', true);
