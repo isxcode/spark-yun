d858cd36de38cc9797dbeb551da8edb6f0eea00e
ispong <ispong@outlook.com>
2025-04-28 15:39:32 +0800

    GH-1740 [Bug] H2数据查询unicode乱码 (#1742)

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/impl/QuerySqlExecutor.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/star/modules/work/run/impl/QuerySqlExecutor.java
@@ -192,7 +192,11 @@ public class QuerySqlExecutor extends WorkExecutor {
             while (resultSet.next()) {
                 metaList = new ArrayList<>();
                 for (int i = 1; i <= columnCount; i++) {
-                    metaList.add(String.valueOf(resultSet.getObject(i)));
+                    try {
+                        metaList.add(resultSet.getString(i));
+                    } catch (Exception e) {
+                        metaList.add(String.valueOf(resultSet.getObject(i)));
+                    }
                 }
                 result.add(metaList);
             }
