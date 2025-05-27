de14746c6bbdad0596bed121b86911cea2095ecd
ispong <ispong@outlook.com>
2025-05-27 18:50:22 +0800

    添加字段标准模块

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/star/api/model/res/ColumnFormatPageRes.java
@@ -0,0 +1,33 @@
+package com.isxcode.star.api.model.res;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.isxcode.star.backend.api.base.serializer.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ColumnFormatPageRes {
+
+    private String id;
+
+    private String name;
+
+    private String tableRule;
+
+    private String parentLayerId;
+
+    private String parentIdList;
+
+    private String parentNameList;
+
+    private String remark;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime createDateTime;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime lastModifiedDateTime;
+}
