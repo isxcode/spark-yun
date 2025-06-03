e1ca655ecf02680d9045c09a07e02c526f6b4ee5
fancinate <1585546519@qq.com>
2025-06-03 22:05:58 +0800

    数据分层开发

---
+++ spark-yun-frontend/src/services/data-layer.service.ts
@@ -0,0 +1,50 @@
+import { http } from '@/utils/http'
+interface SerchParams {
+  page: number
+  pageSize: number
+  searchKeyWord: string
+  parentLayerId?: string
+}
+
+// 列表分页查询
+export function GetDataLayerList(params: SerchParams): Promise<any> {
+  return http.request({
+    method: 'post',
+    url: '/vip/layer/searchLayer',
+    params: params
+  })
+}
+
+// 数据分层树形结构查询
+export function GetDataLayerTreeData(params: SerchParams): Promise<any> {
+    return http.request({
+      method: 'post',
+      url: '/vip/layer/pageLayer',
+      params: params
+    })
+  }
+
+export function SaveDataLayerData(params: SerchParams): Promise<any> {
+  return http.request({
+    method: 'post',
+    url: '/vip/layer/addLayer',
+    params: params
+  })
+}
+
+export function UpdateDataLayerData(params: SerchParams): Promise<any> {
+  return http.request({
+    method: 'post',
+    url: '/vip/layer/updateLayer',
+    params: params
+  })
+}
+
+// 删除
+export function DeleteDataLayerData(params: any): Promise<any> {
+  return http.request({
+    method: 'post',
+    url: '/vip/layer/deleteLayer',
+    params: params
+  })
+}
