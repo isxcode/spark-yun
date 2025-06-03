0a7e14b44e8062a8bde31457c50d484d018a2012
fancinate <1585546519@qq.com>
2025-06-03 22:05:58 +0800

    数据分层开发

---
+++ spark-yun-frontend/src/views/data-planning/data-model/add-modal/index.vue
@@ -0,0 +1,180 @@
+<template>
+    <BlockModal :model-config="modelConfig">
+        <el-form
+            ref="form"
+            class="add-computer-group acquisition-task-add"
+            label-position="top"
+            :model="formData"
+            :rules="rules"
+        >
+            <el-form-item label="分层名称" prop="name">
+                <el-input v-model="formData.name" maxlength="200" placeholder="请输入" />
+            </el-form-item>
+            <el-form-item label="父级分层" prop="parentLayerId">
+                <el-select
+                    :disabled="!!formData.id"
+                    v-model="formData.parentLayerId"
+                    filterable
+                    placeholder="请选择"
+                >
+                    <el-option
+                        v-for="item in parentLayerIdList"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                    />
+                </el-select>
+            </el-form-item>
+            <el-form-item label="表名规范" prop="tableRule">
+                <el-input v-model="formData.tableRule" maxlength="200" placeholder="请输入" />
+            </el-form-item>
+            <el-form-item label="备注">
+                <el-input v-model="formData.remark" type="textarea" maxlength="200"
+                    :autosize="{ minRows: 4, maxRows: 4 }" placeholder="请输入" />
+            </el-form-item>
+        </el-form>
+    </BlockModal>
+</template>
+
+<script lang="ts" setup>
+import { reactive, defineExpose, ref } from 'vue'
+import { GetDataLayerList } from '@/services/data-layer.service'
+import { ElMessage, FormInstance, FormRules } from 'element-plus'
+
+interface Option {
+    label: string
+    value: string
+}
+
+const form = ref<FormInstance>()
+const callback = ref<any>()
+const parentLayerIdList = ref<Option[]>([])
+
+const modelConfig = reactive({
+    title: '添加',
+    visible: false,
+    width: '520px',
+    okConfig: {
+        title: '确定',
+        ok: okEvent,
+        disabled: false,
+        loading: false
+    },
+    cancelConfig: {
+        title: '取消',
+        cancel: closeEvent,
+        disabled: false
+    },
+    needScale: false,
+    zIndex: 1100,
+    closeOnClickModal: false
+})
+const formData = reactive<any>({
+    name: '',
+    parentLayerId: 'currentLevel',
+    tableRule: '',
+    remark: '',
+    id: ''
+})
+const rules = reactive<FormRules>({
+    name: [{ required: true, message: '请输入采集任务名称', trigger: ['blur', 'change'] }],
+    parentLayerId: [{ required: true, message: '请选择父级分层', trigger: ['blur', 'change'] }],
+    tableRule: [{ required: true, message: '请输入表名规范', trigger: ['blur', 'change'] }]
+})
+
+function showModal(cb: () => void, data: any): void {
+    getParentLayerIList()
+    if (data) {
+        Object.keys(formData).forEach((key: string) => {
+            formData[key] = data[key]
+        })
+        modelConfig.title = '编辑'
+    } else {
+        Object.keys(formData).forEach((key: string) => {
+            formData[key] = ''
+            if (key === 'parentLayerId') {
+                formData[key] = 'currentLevel'
+            }
+        })
+        modelConfig.title = '添加'
+    }
+
+    callback.value = cb
+    modelConfig.visible = true
+}
+
+function okEvent() {
+    form.value?.validate((valid: boolean) => {
+        if (valid) {
+            modelConfig.okConfig.loading = true
+            callback.value(formData).then((res: any) => {
+                modelConfig.okConfig.loading = false
+                if (res === undefined) {
+                    modelConfig.visible = false
+                } else {
+                    modelConfig.visible = true
+                }
+            }).catch((err: any) => {
+                modelConfig.okConfig.loading = false
+            })
+        } else {
+            ElMessage.warning('请将表单输入完整')
+        }
+    })
+}
+
+function getParentLayerIList() {
+    GetDataLayerList({
+        page: 0,
+        pageSize: 10000,
+        searchKeyWord: ''
+    }).then((res: any) => {
+        parentLayerIdList.value = [{
+            label: '当前分层',
+            value: 'currentLevel'
+        }, ...res.data.content.map((item: any) => {
+            return {
+                label: item.name,
+                value: item.id
+            }
+        })]
+    }).catch(() => {
+        parentLayerIdList.value = []
+    })
+}
+
+function closeEvent() {
+    modelConfig.visible = false
+}
+
+defineExpose({
+    showModal
+})
+</script>
+
+<style lang="scss">
+.acquisition-task-add {
+    .el-form-item {
+        .el-form-item__content {
+            position: relative;
+            flex-wrap: nowrap;
+            justify-content: space-between;
+
+            .time-num-input {
+                height: 36px;
+
+                .el-input-number__decrease {
+                    top: 16px
+                }
+            }
+        }
+    }
+
+    .cron-config {
+        border: 1px solid getCssVar('border-color');
+        padding: 8px 12px;
+        margin-bottom: 12px;
+        border-radius: 5px;
+    }
+}
+</style>
---
+++ spark-yun-frontend/src/views/data-planning/data-model/list.config.ts
@@ -0,0 +1,96 @@
+export interface BreadCrumb {
+    name: string;
+    code: string;
+    hidden?: boolean;
+}
+
+export interface colConfig {
+    prop?: string;
+    title: string;
+    align?: string;
+    showOverflowTooltip?: boolean;
+    customSlot?: string;
+    width?: number;
+    minWidth?: number;
+    formatter?: any
+    fixed?: string;
+}
+
+export interface Pagination {
+    currentPage: number;
+    pageSize: number;
+    total: number;
+}
+
+export interface TableConfig {
+    tableData: Array<any>;
+    colConfigs: Array<colConfig>;
+    seqType: string;
+    pagination?: Pagination; // 分页数据
+    loading?: boolean; // 表格loading
+}
+
+export const BreadCrumbList: Array<BreadCrumb> = [
+    {
+        name: '数据模型',
+        code: 'data-model'
+    }
+]
+
+export const colConfigs: colConfig[] = [
+    {
+        prop: 'name',
+        title: '名称',
+        minWidth: 125,
+        customSlot: 'nameSlot',
+        showOverflowTooltip: true
+    },
+    {
+        prop: 'tableRule',
+        title: '表名规范',
+        minWidth: 125,
+        showOverflowTooltip: true
+    },
+    {
+        prop: 'parentNameList',
+        title: '父级分层',
+        minWidth: 140,
+        formatter: (data: any) => {
+            return data.cellValue ?? '-'
+        }
+    },
+    {
+        prop: 'remark',
+        title: '备注',
+        minWidth: 120
+    },
+    {
+        prop: 'createByUsername',
+        title: '创建人',
+        minWidth: 120
+    },
+    {
+        prop: 'createDateTime',
+        title: '创建时间',
+        minWidth: 140
+    },
+    {
+        title: '操作',
+        align: 'center',
+        customSlot: 'options',
+        width: 90,
+        fixed: 'right'
+    }
+]
+
+export const TableConfig: TableConfig = {
+    tableData: [],
+    colConfigs: colConfigs,
+    pagination: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 0
+    },
+    seqType: 'seq',
+    loading: false
+}
