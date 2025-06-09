52c9d0fcdb4dcb57b81d91026664bdbd4cffc20d
ispong <ispong@outlook.com>
2025-06-09 11:14:16 +0800

    替换菜单顺序

--- spark-yun-frontend/components.d.ts
+++ spark-yun-frontend/components.d.ts
@@ -41,7 +41,6 @@ declare module 'vue' {
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElOption: typeof import('element-plus/es')['ElOption']
-    ElPageHeader: typeof import('element-plus/es')['ElPageHeader']
     ElPagination: typeof import('element-plus/es')['ElPagination']
     ElPopover: typeof import('element-plus/es')['ElPopover']
     ElRadio: typeof import('element-plus/es')['ElRadio']
