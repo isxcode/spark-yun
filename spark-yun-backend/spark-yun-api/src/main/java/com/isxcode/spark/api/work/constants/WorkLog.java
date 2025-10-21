f7603ecc8ea5094287f06b3ab67622deb60242b1
ispong <ispong@outlook.com>
2025-10-21 20:56:10 +0800

    兼容prql作业类型

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/spark/api/work/constants/WorkLog.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/spark/api/work/constants/WorkLog.java
@@ -2,7 +2,7 @@ package com.isxcode.spark.api.work.constants;
 
 public interface WorkLog {
 
-    String SUCCESS_INFO = " INFO  : ";
+    String SUCCESS_INFO = "  INFO  : ";
 
     String ERROR_INFO = " ERROR : ";
 }
