e6e38c1c7c7be6a70958e9ddee728419d2426a20
ispong <ispong@outlook.com>
2025-11-24 16:33:24 +0800

    获取字段血缘关系

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/spark/api/meta/ao/MetaColumnLinageAo.java
@@ -0,0 +1,51 @@
+package com.isxcode.spark.api.meta.ao;
+
+import lombok.Data;
+
+@Data
+public class MetaColumnLinageAo {
+
+    private String lineageId;
+
+    private String fromDbId;
+
+    private String fromTableName;
+
+    private String fromColumnName;
+
+    private String toDbId;
+
+    private String toTableName;
+
+    private String toColumnName;
+
+    private String dbType;
+
+    private String dbName;
+
+    private String remark;
+
+    private String workVersionId;
+
+    private String workId;
+
+    private String workName;
+
+    public MetaColumnLinageAo(String lineageId, String fromDbId, String fromTableName, String fromColumnName,
+        String toDbId, String toTableName, String toColumnName, String dbType, String dbName, String remark,
+        String workVersionId, String workId, String workName) {
+        this.lineageId = lineageId;
+        this.fromDbId = fromDbId;
+        this.fromTableName = fromTableName;
+        this.fromColumnName = fromColumnName;
+        this.toDbId = toDbId;
+        this.toTableName = toTableName;
+        this.toColumnName = toColumnName;
+        this.dbType = dbType;
+        this.dbName = dbName;
+        this.remark = remark;
+        this.workVersionId = workVersionId;
+        this.workId = workId;
+        this.workName = workName;
+    }
+}
