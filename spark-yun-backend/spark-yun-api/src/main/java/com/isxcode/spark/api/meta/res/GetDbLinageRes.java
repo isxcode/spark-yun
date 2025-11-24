941d60f08e2b36eaad86d3e310205e56707602e2
ispong <ispong@outlook.com>
2025-11-24 15:32:58 +0800

    支持DB血缘查询

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/spark/api/meta/res/GetDbLinageRes.java
@@ -0,0 +1,25 @@
+package com.isxcode.spark.api.meta.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GetDbLinageRes {
+
+    private String dbId;
+
+    private String dbName;
+
+    private String dbType;
+
+    private List<GetDbLinageRes> sonDbLineageList;
+
+    private List<GetDbLinageRes> parentDbLineageList;
+}
