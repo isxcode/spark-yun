cedaf3b3b330438888a283962c845237ab5e83e7
ispong <ispong@outlook.com>
2026-01-15 15:16:33 +0800

    一键生成目标表初步

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/spark/api/datasource/constants/ColumnCode.java
@@ -0,0 +1,31 @@
+package com.isxcode.spark.api.datasource.constants;
+
+/**
+ * 字段类型.
+ */
+public interface ColumnCode {
+
+    String BOOLEAN = "BOOLEAN";
+
+    String INT = "INT";
+
+    String BIGINT = "BIGINT";
+
+    String CHAR = "CHAR";
+
+    String STRING = "STRING";
+
+    String DATE = "DATE";
+
+    String DATETIME = "DATETIME";
+
+    String TIMESTAMP = "TIMESTAMP";
+
+    String DOUBLE = "DOUBLE";
+
+    String FLOAT = "FLOAT";
+
+    String TEXT = "TEXT";
+
+    String DECIMAL = "DECIMAL";
+}
