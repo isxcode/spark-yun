d95326060dda4a33b0236c7ecbe6c6a88b8704de
ispong <ispong@outlook.com>
2026-01-16 16:47:03 +0800

    支持版本号自动替换

--- spark-yun-frontend/src/main.ts
+++ spark-yun-frontend/src/main.ts
@@ -14,6 +14,9 @@ import '@/assets/styles/global.scss'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import VueGridLayout from 'vue-grid-layout'
 
+// 打印版本号
+console.log(`%c 至轻云 %c ${__APP_VERSION__} `, 'background:#35495e; padding: 1px; border-radius: 3px 0 0 3px; color: #fff', 'background:#41b883; padding: 1px; border-radius: 0 3px 3px 0; color: #fff')
+
 const app = createApp(App)
 for (const [ key, component ] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
