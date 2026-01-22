a0436a0083f1837fd0eb8063fa1c17ff989556ce
ispong <ispong@outlook.com>
2026-01-22 18:36:07 +0800

    支持转换组件

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/spark/api/work/constants/EtlTransformWay.java
@@ -0,0 +1,17 @@
+package com.isxcode.spark.api.work.constants;
+
+/**
+ * etl转换方式.
+ */
+public interface EtlTransformWay {
+
+    /**
+     * 自定义转换.
+     */
+    String CUSTOM_TRANSFORM = "CUSTOM_TRANSFORM";
+
+    /**
+     * 函数转换.
+     */
+    String FUNCTION_TRANSFORM = "FUNCTION_TRANSFORM";
+}
