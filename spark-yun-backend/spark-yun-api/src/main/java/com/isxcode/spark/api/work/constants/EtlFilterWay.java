cdd0722f36d6a8a137a9eb87f5096f41c608134f
ispong <ispong@outlook.com>
2026-01-23 11:44:10 +0800

    支持数据过滤组件

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/spark/api/work/constants/EtlFilterWay.java
@@ -0,0 +1,17 @@
+package com.isxcode.spark.api.work.constants;
+
+/**
+ * etl过滤条件.
+ */
+public interface EtlFilterWay {
+
+    /**
+     * 且.
+     */
+    String AND = "AND";
+
+    /**
+     * 或.
+     */
+    String OR = "OR";
+}
