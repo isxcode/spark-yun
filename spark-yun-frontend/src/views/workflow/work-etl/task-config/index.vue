9f8deb219e2865e24c60b93771c5e12316cd7b2b
ispong <ispong@outlook.com>
2026-03-24 16:19:44 +0800

    优化数据输入弹窗

--- spark-yun-frontend/src/views/workflow/work-etl/task-config/index.vue
+++ spark-yun-frontend/src/views/workflow/work-etl/task-config/index.vue
@@ -69,7 +69,7 @@ const formInstance = shallowRef<any>(Components)
 const modelConfig = reactive({
     title: '配置详情',
     visible: false,
-    width: '60%',
+    width: '30%',
     customClass: 'etl-task-config',
     okConfig: {
         title: '确定',
