a2537557a9f84f2ef4ec8c2becf1e256d313aa5e
ispong <ispong@outlook.com>
2026-03-26 17:20:36 +0800

    支持顺序拖拽

--- spark-yun-frontend/src/views/workflow/work-etl/task-config/output-modal/default-output/index.vue
+++ spark-yun-frontend/src/views/workflow/work-etl/task-config/output-modal/default-output/index.vue
@@ -3,6 +3,7 @@
         <div style="max-height: 444px;">
             <BlockTable
               :table-config="tableConfig"
+              @rowDragendEvent="onRowDragend"
             >
                 <template #checkboxHeaderSlot>
                     <el-checkbox :model-value="isAllChecked" @change="toggleSelectAll" />
@@ -64,7 +65,8 @@ const baseColConfigs = [
         prop: 'colName',
         title: '字段名',
         minWidth: 120,
-        showOverflowTooltip: true
+        showOverflowTooltip: true,
+        dragSort: true
     },
     {
         prop: 'colType',
@@ -160,6 +162,11 @@ function editCode(row: any, index: number) {
 }
 
 // 删除来源编码
+function onRowDragend(e: any) {
+    tableConfig.tableData = e.tableData
+    updateAllChecked()
+}
+
 function removeCode(scopeSlot: any) {
     ElMessageBox.confirm('确定删除该字段吗？', '警告', {
         confirmButtonText: '确定',
