e91bff2eb1ac3685c411284073efeed3227bdb01
ispong <ispong@outlook.com>
2026-03-26 10:16:34 +0800

    ai优化14

---
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/spark/api/work/constants/EtlAddColType.java
@@ -0,0 +1,17 @@
+package com.isxcode.spark.api.work.constants;
+
+/**
+ * etl新建字段条件.
+ */
+public interface EtlAddColType {
+
+    /**
+     * 来源表.
+     */
+    String SOURCE_TABLE = "SOURCE_TABLE";
+
+    /**
+     * 手动添加.
+     */
+    String MANUAL = "MANUAL";
+}
