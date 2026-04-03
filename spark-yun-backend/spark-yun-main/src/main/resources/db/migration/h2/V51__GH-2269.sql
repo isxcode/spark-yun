ebe5f76236cff950056af3d99e129a3414c12502
ispong <ispong@outlook.com>
2026-04-03 18:07:37 +0800

    兼容doris数据库

--- spark-yun-backend/spark-yun-main/src/main/resources/db/migration/h2/V51__GH-2269.sql
+++ spark-yun-backend/spark-yun-main/src/main/resources/db/migration/h2/V51__GH-2269.sql
@@ -5,3 +5,7 @@ alter table SY_WORK_CONFIG
 -- 版本表中添加新增Flink数据同步配置字段
 alter table SY_WORK_VERSION
     add SYNC_FLINK_CONFIG longtext;
+
+-- doris数据源特有字段feNodes
+alter table SY_DATASOURCE
+    add FE_NODES varchar(200);
