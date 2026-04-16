67fa9f708e170616f4c26ebf4ba3d10d68918e9e
ispong <ispong@outlook.com>
2026-04-16 19:08:51 +0800

    格式化代码

--- spark-yun-frontend/components.d.ts
+++ spark-yun-frontend/components.d.ts
@@ -38,7 +38,7 @@ declare module 'vue' {
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
-    EllipsisTooltip: typeof import('./src/components/ellipsis-tooltip/ellipsis-tooltip.ts')['default']
+    EllipsisTooltip: typeof import('./src/components/ellipsis-tooltip/ellipsis-tooltip.vue')['default']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElOption: typeof import('element-plus/es')['ElOption']
