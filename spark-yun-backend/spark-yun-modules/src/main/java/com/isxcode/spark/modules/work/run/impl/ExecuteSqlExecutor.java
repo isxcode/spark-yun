9310b3f1e6a7026980ae8e52f98d4d5a1a8b3120
ispong <ispong@outlook.com>
2026-05-07 18:53:31 +0800

    格式化代码

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/spark/modules/work/run/impl/ExecuteSqlExecutor.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/spark/modules/work/run/impl/ExecuteSqlExecutor.java
@@ -66,14 +66,14 @@ public class ExecuteSqlExecutor extends WorkExecutor {
     private final ServerProperties serverProperties;
 
     public ExecuteSqlExecutor(WorkInstanceRepository workInstanceRepository,
-                              WorkflowInstanceRepository workflowInstanceRepository, DatasourceRepository datasourceRepository,
-                              SqlCommentService sqlCommentService, SqlValueService sqlValueService, SqlFunctionService sqlFunctionService,
-                              AlarmService alarmService, DataSourceFactory dataSourceFactory, DatasourceMapper datasourceMapper,
-                              WorkEventRepository workEventRepository, Locker locker, WorkRepository workRepository,
-                              WorkRunJobFactory workRunJobFactory, WorkConfigRepository workConfigRepository,
-                              VipWorkVersionRepository vipWorkVersionRepository, WorkService workService, DatasourceService datasourceService,
-                              IsxAppProperties isxAppProperties, ServerProperties serverProperties,
-                              MetaColumnLineageService metaColumnLineageService) {
+        WorkflowInstanceRepository workflowInstanceRepository, DatasourceRepository datasourceRepository,
+        SqlCommentService sqlCommentService, SqlValueService sqlValueService, SqlFunctionService sqlFunctionService,
+        AlarmService alarmService, DataSourceFactory dataSourceFactory, DatasourceMapper datasourceMapper,
+        WorkEventRepository workEventRepository, Locker locker, WorkRepository workRepository,
+        WorkRunJobFactory workRunJobFactory, WorkConfigRepository workConfigRepository,
+        VipWorkVersionRepository vipWorkVersionRepository, WorkService workService, DatasourceService datasourceService,
+        IsxAppProperties isxAppProperties, ServerProperties serverProperties,
+        MetaColumnLineageService metaColumnLineageService) {
 
         super(alarmService, locker, workRepository, workInstanceRepository, workflowInstanceRepository,
             workEventRepository, workRunJobFactory, sqlFunctionService, workConfigRepository, vipWorkVersionRepository,
