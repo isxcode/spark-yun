b583b2e6b12e4a5468a9166fbdb656e8143a4cef
ispong <ispong@outlook.com>
2026-05-07 18:53:31 +0800

    格式化代码

--- spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/spark/modules/work/run/impl/QuerySqlExecutor.java
+++ spark-yun-backend/spark-yun-modules/src/main/java/com/isxcode/spark/modules/work/run/impl/QuerySqlExecutor.java
@@ -62,13 +62,13 @@ public class QuerySqlExecutor extends WorkExecutor {
     private final IsxAppProperties isxAppProperties;
 
     public QuerySqlExecutor(WorkInstanceRepository workInstanceRepository,
-                            WorkflowInstanceRepository workflowInstanceRepository, DatasourceRepository datasourceRepository,
-                            SqlCommentService sqlCommentService, SqlValueService sqlValueService, SqlFunctionService sqlFunctionService,
-                            AlarmService alarmService, DataSourceFactory dataSourceFactory, DatasourceMapper datasourceMapper,
-                            WorkEventRepository workEventRepository, Locker locker, WorkRepository workRepository,
-                            WorkRunJobFactory workRunJobFactory, WorkConfigRepository workConfigRepository,
-                            VipWorkVersionRepository vipWorkVersionRepository, WorkService workService, DatasourceService datasourceService,
-                            IsxAppProperties isxAppProperties, MetaColumnLineageService metaColumnLineageService) {
+        WorkflowInstanceRepository workflowInstanceRepository, DatasourceRepository datasourceRepository,
+        SqlCommentService sqlCommentService, SqlValueService sqlValueService, SqlFunctionService sqlFunctionService,
+        AlarmService alarmService, DataSourceFactory dataSourceFactory, DatasourceMapper datasourceMapper,
+        WorkEventRepository workEventRepository, Locker locker, WorkRepository workRepository,
+        WorkRunJobFactory workRunJobFactory, WorkConfigRepository workConfigRepository,
+        VipWorkVersionRepository vipWorkVersionRepository, WorkService workService, DatasourceService datasourceService,
+        IsxAppProperties isxAppProperties, MetaColumnLineageService metaColumnLineageService) {
 
         super(alarmService, locker, workRepository, workInstanceRepository, workflowInstanceRepository,
             workEventRepository, workRunJobFactory, sqlFunctionService, workConfigRepository, vipWorkVersionRepository,
@@ -90,7 +90,7 @@ public class QuerySqlExecutor extends WorkExecutor {
 
     @Override
     protected String execute(WorkRunContext workRunContext, WorkInstanceEntity workInstance,
-                             WorkEventEntity workEvent) {
+        WorkEventEntity workEvent) {
 
         // 获取日志
         StringBuilder logBuilder = new StringBuilder(workInstance.getSubmitLog());
