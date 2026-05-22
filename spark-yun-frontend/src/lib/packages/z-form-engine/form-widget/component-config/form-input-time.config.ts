b2f801e3828223649d8317100811c4565e01b5aa
ispong <ispong@outlook.com>
2026-05-22 11:05:03 +0800

    添加字段类型

---
+++ spark-yun-frontend/src/lib/packages/z-form-engine/form-widget/component-config/form-input-time.config.ts
@@ -0,0 +1,32 @@
+export default {
+    editConfig: {
+        name: '时间选择',
+        icon: 'Clock',
+        code: 'FormInputTime'
+    },
+    componentConfig: {
+        uuid: '16 uuid',
+        type: 'simple',
+        formValueCode: '',
+        codeType: 'custom',
+        label: '时间选择',
+        placeholder: '请选择',
+        disabled: false,
+        required: false,
+        isColumn: true,
+        width: 2,
+        componentType: 'FormInputTime',
+        valid: true
+    },
+    conponentSetConfig: [
+        'LABEL',
+        'CODE_SELECT',
+        'PRIMARY_COLUMN',
+        'WIDTH',
+        'DEFAULTVALUE_TIME',
+        'PLACEHOLDER',
+        'DISABLED',
+        'REQUIRED',
+        'LIST_COLUMN'
+    ]
+}
