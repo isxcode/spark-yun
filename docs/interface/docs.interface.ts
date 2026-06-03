1d5b10ddcdbddcc8ce73d573d11a7e1ffb160772
ispong <ispong@outlook.com>
2026-06-03 15:58:56 +0800

    移动check配置目录和docs目录

--- docs/interface/docs.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface DirNode {
-  id: number;
-  title: string;
-  domId: string;
-  hLevel: number;
-  pid?: number;
-  isActive?: boolean,
-  children?: DirNode[];
-}
--- docs/plugins/svgicon.client.ts
+++
@@ -1,5 +0,0 @@
-import svgIcon from "@/components/svg-icon.vue";
-import 'virtual:svg-icons-register'
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component("svg-icon", svgIcon);
-});
